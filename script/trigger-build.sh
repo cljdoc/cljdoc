41f84950abdad53747494f592a79d06d8d2f6c6f
Martin Klepsch <martinklepsch@googlemail.com>
2018-01-29 17:34:18 +0100

    add some basic documentation to trigger-build.sh

--- script/trigger-build.sh
+++ script/trigger-build.sh
@@ -5,8 +5,13 @@ project="$1"
 version="$2"
 jar_path="$3"
 
+# $CIRCLE_BUILDER_PROJECT must be Circle CI project set up
+# with the configuration that can be found in the cljdoc-builder project
+# https://github.com/martinklepsch/cljdoc-builder
+# For the repo above the value should be "github/martinklepsch/cljdoc-builder"
+
 curl -u "$CIRCLE_API_TOKEN:" \
      -d build_parameters[CLJDOC_PROJECT]="$project" \
      -d build_parameters[CLJDOC_PROJECT_VERSION]="$version" \
      -d build_parameters[CLJDOC_PROJECT_JAR]="$jar_path" \
-     "$CIRCLE_BUILDER_ENDPOINT"
+     "https://circleci.com/api/v1.1/project/$CIRCLE_BUILDER_PROJECT/tree/master"
