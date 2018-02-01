eccdc2724a79ede6a91768b5a42d34162ee4d367
Martin Klepsch <martinklepsch@googlemail.com>
2018-02-01 18:42:02 +0100

    ops: add basick packer config to create DO snapshots

---
+++ ops/image/provision.sh
@@ -0,0 +1,49 @@
+#!/usr/bin/env bash
+
+set -euo pipefail
+
+username="cljdoc"
+sudo add-apt-repository -y ppa:webupd8team/java
+
+apt-get update
+apt-get upgrade -y
+apt-get install -y git curl
+
+echo "oracle-java8-installer shared/accepted-oracle-license-v1-1 select true" | sudo debconf-set-selections
+apt-get install -y oracle-java8-installer
+
+# User setup
+
+adduser --disabled-password --gecos "" "$username"
+usermod -aG sudo "$username"
+
+# SSH
+mkdir /home/$username/.ssh
+
+curl -s "https://github.com/martinklepsch.keys" >> /home/$username/.ssh/authorized_keys
+
+chmod 700 /home/$username/.ssh
+chmod 600 /home/$username/.ssh/authorized_keys
+chown -R $username:$username /home/$username/.ssh
+
+# allow passwordless sudo
+# echo "$username ALL=(ALL) NOPASSWD:ALL" >> /etc/sudoers
+
+# Disable SSH PasswordAuthentication
+# This will lock out the root user unless a pubkey has been added to it's authorized_keys file
+# TODO actually it does not, see https://www.digitalocean.com/community/questions/why-is-there-passwordauthentication-yes-in-sshd_config-even-though-i-changed-it-when-creating-the-image
+echo "Disabling SSH PasswordAuthentication"
+sed -i '/PasswordAuthentication yes/c\PasswordAuthentication no' /etc/ssh/sshd_config
+cat /etc/ssh/sshd_config
+
+# Consider adding Firewall setup
+# https://www.digitalocean.com/community/tutorials/initial-server-setup-with-ubuntu-16-04#step-seven-%E2%80%94-set-up-a-basic-firewall
+
+
+# Application Setup
+
+# bootpath=/usr/local/bin/boot
+# curl -sL https://github.com/boot-clj/boot-bin/releases/download/2.7.2/boot.sh -o "$bootpath"
+# chmod +x "$bootpath"
+
+# BOOT_VERSION=2.7.2 boot repl
