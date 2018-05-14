67a307bb021bbebbdd7bc984f1a9d4cc4440cc90
Martin Klepsch <martinklepsch@googlemail.com>
2018-05-14 12:55:39 +0200

    add tools.logging to default analysis deps

--- src/cljdoc/analysis/task.clj
+++ src/cljdoc/analysis/task.clj
@@ -22,6 +22,7 @@
     [org.clojure/tools.namespace "0.2.11"]
     [org.clojure/clojurescript "1.9.946"] ; Codox depends on old CLJS which fails with CLJ 1.9
     [org.clojure/core.async "RELEASE"] ; Manifold dev-dependency — we should probably detect+load these
+    [org.clojure/tools.logging "RELEASE"] ; Pedestal Interceptors dev-dependency — we should probably detect+load these
     [codox "0.10.3" :exclusions [enlive hiccup org.pegdown/pegdown]]])
 
 (defn copy [uri file]
