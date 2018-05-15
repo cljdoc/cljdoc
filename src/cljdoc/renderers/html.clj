f231a7db347bc9904c72802149abd83b6884e001
Martin Klepsch <martinklepsch@googlemail.com>
2018-05-15 13:17:28 +0200

    move on-clojars? helper out of util namespace (because that is loaded in analyzer pod)

--- src/cljdoc/renderers/html.clj
+++ src/cljdoc/renderers/html.clj
@@ -393,11 +393,18 @@
     (assert (and dir (.isDirectory dir)) (format "HTMLRenderer expects output directory, was %s" dir))
     (write-docs* cache-bundle dir)))
 
+(defn- on-clojars? [coordinate]
+  ;; TODO move elsewhere as this won't load in analysis env
+  (try
+    (= 200 (:status @(aleph.http/head (cljdoc.util/remote-jar-file coordinate))))
+    (catch clojure.lang.ExceptionInfo e
+      false)))
+
 (defn request-build-page [route-params]
   (->> [:div.pa4-ns.pa2
         [:h1 "Want to build some documentation?"]
         [:p "We currently don't have documentation built for " (clojars-id route-params) " v" (:version route-params)]
-        (if (cljdoc.util/on-clojars? [(clojars-id route-params) (:version route-params)])
+        (if (on-clojars? [(clojars-id route-params) (:version route-params)])
           [:form.pv3 {:action "/api/request-build2" :method "POST"}
            [:input.pa2.mr2.br2.ba.outline-0.blue {:type "text" :id "project" :name "project" :value (str (:group-id route-params) "/" (:artifact-id route-params))}]
            [:input.pa2.mr2.br2.ba.outline-0.blue {:type "text" :id "version" :name "version" :value (:version route-params)}]
