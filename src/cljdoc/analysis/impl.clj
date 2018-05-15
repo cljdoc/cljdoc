5c1166692e102ace2513a03a89c4c83a94022301
Martin Klepsch <martinklepsch@googlemail.com>
2018-05-15 13:14:56 +0200

    use plain printf logging in analysis namespaces

--- src/cljdoc/analysis/impl.clj
+++ src/cljdoc/analysis/impl.clj
@@ -1,6 +1,5 @@
 (ns cljdoc.analysis.impl
-  (:require [clojure.tools.logging :as log]
-            [codox.main]
+  (:require [codox.main]
             [cljs.util]))
 
 (defn codox-config [namespaces jar-contents-path platf]
@@ -25,7 +24,7 @@
                                   (nil? (:arglists %)))
                            ;; Instaparse 1.4.9 and other projects exhibited
                            ;; this issue. Might be an inconsistency in Codox?
-                           (do (log/warnf "Removing nil :arglists from %s" %)
+                           (do (printf "WARNING Removing nil :arglists from %s" %)
                                (dissoc % :arglists))
                            %)]
     (-> cdx-namespace
@@ -35,9 +34,9 @@
 (defn codox-namespaces [namespaces jar-contents-path platf]
   (let [config (codox-config namespaces jar-contents-path platf)]
     ;; TODO print versions for Clojure/CLJS and other important deps
-    (boot.util/info "Analysing sources for platform %s\n" (pr-str platf))
-    (boot.util/dbug "ClojureScript version %s\n" (cljs.util/clojurescript-version))
-    (boot.util/dbug "Codox opts: %s\n" config)
+    (printf "Analysing sources for platform %s\n" (pr-str platf))
+    (printf "ClojureScript version %s\n" (cljs.util/clojurescript-version))
+    (printf "Codox opts: %s\n" config)
     (->> (codox.main/generate-docs config)
          :namespaces
          (mapv sanitize-cdx)
