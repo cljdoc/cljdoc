06dbb29e4b85ddd57e6501d8925b83860c0899e2
Martin Klepsch <martinklepsch@googlemail.com>
2018-05-31 14:20:28 +0200

    split out git-specific stuff of ingest function

--- src/cljdoc/server/ingest.clj
+++ src/cljdoc/server/ingest.clj
@@ -18,6 +18,51 @@
      :artifact-id (util/artifact-id project)
      :version version})))
 
+(defn analyze-git-repo [project version scm-url pom-revision]
+  (let [git-dir (cljdoc.util/system-temp-dir (str "git-" project))]
+    (try
+      (log/info "Cloning Git repo" scm-url)
+      (git/clone scm-url git-dir)
+
+      ;; Stuff that depends on a SCM url being present
+      (let [repo        (git/->repo git-dir)
+            version-tag (git/version-tag repo version)
+            revision    (or (:name version-tag) pom-revision
+                            (when (.endsWith version "-SNAPSHOT")
+                              "master"))
+            config-edn  (when revision
+                          (->> (or (git/read-cljdoc-config repo revision)
+                                   ;; in case people add the file later,
+                                   ;; also check in master branch
+                                   (git/read-cljdoc-config repo "master"))
+                               (clojure.edn/read-string)))]
+
+        (if-not revision
+          (do (log/warnf "No revision found for version %s in %s\n" version scm-url)
+              (telegram/no-version-tag project version scm-url))
+
+          {:scm      {:files (git/ls-files repo revision)
+                      :url scm-url
+                      :commit pom-revision
+                      :tag version-tag}
+           :doc-tree (doctree/process-toc
+                      (fn slurp-at-rev [f]
+                        ;; We are intentionally relaxed here for now
+                        ;; In principle we should only look at files at the tagged
+                        ;; revision but if a file has been added after the tagged
+                        ;; revision we might as well include it to allow a smooth,
+                        ;; even if slightly less correct, UX
+                        (if revision
+                          (git/slurp-file-at repo revision f)
+                          (git/slurp-file-at repo "master" f)))
+                      (or (:cljdoc.doc/tree config-edn)
+                          (get-in cljdoc.util/hardcoded-config
+                                  [(cljdoc.util/normalize-project project) :cljdoc.doc/tree])
+                          (doctree/derive-toc git-dir)))}))
+      (finally
+        (when (.exists git-dir)
+          (cljdoc.util/delete-directory! git-dir))))))
+
 (defn ingest-cljdoc-edn
   "Ingest all the information in the passed `cljdoc-edn` data.
 
@@ -29,13 +74,13 @@
   - store articles and other version specific data in grimoire
   - store API data in grimoire"
   [data-dir cljdoc-edn]
+  {:post [(find % :scm-url)]}
   (let [pom-doc      (pom/parse (:pom-str cljdoc-edn))
         artifact     (pom/artifact-info pom-doc)
         scm-info     (pom/scm-info pom-doc)
         project      (str (:group-id artifact) "/" (:artifact-id artifact))
         version      (:version artifact)
         v-thing      (cljdoc.grimoire-helpers/version-thing project version)
-        git-dir      (io/file data-dir (cljdoc.util/git-dir project version))
         store        (cljdoc.grimoire-helpers/grimoire-store
                       (doto (io/file data-dir "grimoire") (.mkdir)))
         scm-url      (some-> (or (:url scm-info)
@@ -55,66 +100,28 @@
         :codox        (:codox cljdoc-edn)
         :store        store})
 
-      (if-not scm-url
-        (do
-          (log/warnf "Could not find SCM URL for project %s v%s" project version)
-          (done project version))
-
-        (do
-          (log/info "Cloning Git repo" scm-url)
-          (git/clone scm-url git-dir)
-
-          ;; Stuff that depends on a SCM url being present
-          (let [repo        (git/->repo git-dir)
-                version-tag (git/version-tag repo version)
-                revision    (or (:name version-tag) (:sha scm-info)
-                                (when (.endsWith version "-SNAPSHOT")
-                                  "master"))
-                pom-scm-sha (:sha scm-info)
-                config-edn  (->> (or (git/read-cljdoc-config repo revision)
-                                     ;; in case people add the file later,
-                                     ;; also check in master branch
-                                     (git/read-cljdoc-config repo "master"))
-                                 (clojure.edn/read-string))]
-
-            (when-not version-tag
-              (do (log/warnf "No version tag found for version %s in %s\n" version scm-url)
-                  (telegram/no-version-tag project version scm-url)))
-
+      (let [git-analysis (and (some? scm-url)
+                              (analyze-git-repo project version scm-url (:sha scm-info)))]
+        (if git-analysis
+          (do
             (log/info "Importing Articles into Grimoire")
             (cljdoc.grimoire-helpers/import-doc
              {:version      v-thing
               :store        store
               :jar          {}
-              :scm          {:files (git/ls-files repo revision)
-                             :url scm-url
-                             :commit pom-scm-sha
-                             :tag (git/version-tag repo version)}
-              :doc-tree     (doctree/process-toc
-                             (fn slurp-at-rev [f]
-                               ;; We are intentionally relaxed here for now
-                               ;; In principle we should only look at files at the tagged
-                               ;; revision but if a file has been added after the tagged
-                               ;; revision we might as well include it to allow a smooth,
-                               ;; even if slightly less correct, UX
-                               (or (git/slurp-file-at repo revision f)
-                                   (git/slurp-file-at repo "master" f)))
-                             (or (:cljdoc.doc/tree config-edn)
-                                 (get-in cljdoc.util/hardcoded-config
-                                         [(cljdoc.util/normalize-project project) :cljdoc.doc/tree])
-                                 (doctree/derive-toc git-dir)))})
+              :scm          (:scm git-analysis)
+              :doc-tree     (:doc-tree git-analysis)})
 
             (done project version)
-
             {:scm-url scm-url
-             :commit (or (:commit version-tag) pom-scm-sha)})))
+             :commit  (-> git-analysis :scm :commit)})
 
+          (do
+            (done project version)
+            {:scm-url scm-url})))
 
       (catch Throwable t
-        (throw (ex-info "Exception while running full build" {:project project :version version} t)))
-      (finally
-        (when (.exists git-dir)
-          (cljdoc.util/delete-directory! git-dir))))))
+        (throw (ex-info "Exception while running full build" {:project project :version version} t))))))
 
 (comment
 
