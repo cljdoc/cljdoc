405d9937317fe45763ba96e7b5cfb564f3726673
Martin Klepsch <martinklepsch@googlemail.com>
2018-06-01 16:30:47 +0200

    nginx-docker: expand readme

--- ops/docker/nginx/README.md
+++ ops/docker/nginx/README.md
@@ -12,3 +12,15 @@ docker build -t cljdoc-nginx .
 docker run --name cljdoc-nginx -p 80:80 -p 443:443 -d --rm cljdoc-nginx
 ```
 
+- [ ] use terraform to create a user with [this policy](https://github.com/certbot/certbot/blob/master/certbot-dns-route53/examples/sample-aws-policy.json) for DNS based certbot auth
+- [ ] figure out how to provide AWS creds to `get-cert.sh` and run it on the instance
+- [ ] some lines in cljdoc.xyz.conf cause nginx to fall over (ssl related)
+- [ ] it's not clear to me yet how to access logs of nginx cljdoc server block
+
+### Certificates
+
+Getting certificates with nginx running in docker is different and I
+couldn't quite get it to work.  Somewhere down this road I discovered
+that certbot provides DNS challenge plugins as Docker containers and
+because this works without any running web server it's significantly
+easier to get a certificate.
--- ops/get-cert.sh
+++ ops/get-cert.sh
@@ -2,16 +2,46 @@
 
 set -eou pipefail
 
+email="martinklepsch@gmail.com"
 domain="cljdoc.xyz"
 
 echo "--- Getting Certificate -------------------------------------------------------"
 
-certbot certonly -m martinklepsch@gmail.com -d "$domain" --nginx --agree-tos --non-interactive
+export AWS_ACCESS_KEY_ID="FIXME"
+export AWS_SECRET_ACCESS_KEY="FIXME"
 
-echo "--- Linking Server Block ------------------------------------------------------"
+# certbot certonly -m "$email" -d "$domain" --nginx --agree-tos --non-interactive
 
-ln -s "/etc/nginx/sites-available/$domain.conf" "/etc/nginx/sites-enabled/$domain.conf"
+# Using certbot with route53 requires building certbot manually
+# certbot certonly -m "$email" -d "$domain" --dns-route53 --agree-tos --non-interactive
+
+# So here we go and use their docker images -----------------------------------------
+
+# Permissions can be fixed by appending :Z
+# https://stackoverflow.com/a/34537509
+# -v /tmp/le:/tmp/:Z
+
+# TODO figure out if this line is necessary:
+# -v /etc/letsencrypt-lib/:/var/lib/letsencrypt-d \
+
+docker run -it --rm \
+       --name certbot \
+       -v /tmp/letsencrypt:/etc/letsencrypt/:Z \
+       -v /etc/letsencrypt-lib/:/var/lib/letsencrypt-d \
+       -e AWS_ACCESS_KEY_ID="$AWS_ACCESS_KEY_ID" \
+       -e AWS_SECRET_ACCESS_KEY="$AWS_SECRET_ACCESS_KEY" \
+       certbot/dns-route53 certonly -m "$email" --dns-route53 -d "$domain" --agree-tos --non-interactive
+
+echo "--- Moving certs in right place -----------------------------------------------"
+
+mv "/tmp/letsencrypt/live/$domain" /etc/letsencrypt/live/
+
+echo "--- Clean up docker mount -----------------------------------------------------"
+
+rm -rf /tmp/letsencrypt
 
 echo "--- Restarting Nginx ----------------------------------------------------------"
+
+# TODO fix this
 
 systemctl restart nginx
