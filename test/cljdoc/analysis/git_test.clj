b1a07159ab89e75d562df476f303ac702a48dfad
Martin Klepsch <martinklepsch@googlemail.com>
2018-06-04 11:38:46 +0200

    add some basic test for git repo analysis

---
+++ test/cljdoc/analysis/git_test.clj
@@ -0,0 +1,15 @@
+(ns cljdoc.analysis.git-test
+  (:require [cljdoc.analysis.git :as git-ana]
+            [clojure.test :as t]))
+
+(t/deftest version-tag
+  (let [analysis (git-ana/analyze-git-repo 'metosin/reitit "0.1.1" "https://github.com/metosin/reitit" nil)]
+    (t/is (= "0.1.1" (-> analysis :scm :tag :name)))
+    ;; This will fail if the tag is modified (it shouldn't be)
+    (t/is (= "c210e7f1c2f8a163676c8e3abeab8e50951458bb"
+             (-> analysis :scm :tag :commit)))))
+
+(comment
+  (t/run-tests)
+
+  )
