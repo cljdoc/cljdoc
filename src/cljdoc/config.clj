e5db8a37be722bfb5106c468ce195b192cb17a1d
Martin Klepsch <martinklepsch@googlemail.com>
2018-06-05 12:36:08 +0200

    use default dev-config file for CI

--- src/cljdoc/config.clj
+++ src/cljdoc/config.clj
@@ -15,7 +15,7 @@
   (if (some? (clojure.core/get-in config-map ks))
     (clojure.core/get-in config-map ks)
     (throw (ex-info (format "No config found for path %s\nDid you configure your secrets.edn file?" ks)
-                    {:ks ks, :profile (profile), :config config-map}))))
+                    {:ks ks, :profile (profile)}))))
 
 (defn config []
   (aero/read-config (io/resource "config.edn") {:profile (profile)}))
