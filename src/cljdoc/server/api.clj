6022dee83c4dab12df344bfc5ec1b4fab79789e4
Martin Klepsch <martinklepsch@googlemail.com>
2018-06-06 16:05:41 +0200

    pass correct argument :sweat_smile:

--- src/cljdoc/server/api.clj
+++ src/cljdoc/server/api.clj
@@ -61,7 +61,7 @@
     (if (analysis-service/circle-ci? analysis-service)
       (let [build-num (-> ana-resp :body json/parse-string (get "build_num"))
             job-url   (str "https://circleci.com/gh/martinklepsch/cljdoc-builder/" build-num)]
-        (telegram/build-requested project version job-url)
+        (telegram/build-requested project version build-id)
         (when (= 201 (:status ana-resp))
           (assert build-num "build number missing from CircleCI response")
           (build-log/analysis-kicked-off! build-tracker build-id job-url)
