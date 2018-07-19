82c2196911bcb90d2e947b5690c39599e0a53fff
Martin Klepsch <martinklepsch@googlemail.com>
2018-07-19 17:09:42 +0200

    add various more links to github-url helper

--- modules/shared-utils/src/cljdoc/util.clj
+++ modules/shared-utils/src/cljdoc/util.clj
@@ -141,14 +141,20 @@
   (str (gh-owner gh-url) "/" (gh-repo gh-url)))
 
 (defn github-url [type]
-  (let [base "https://github.com/martinklepsch/cljdoc"]
+  (let [base "https://github.com/cljdoc/cljdoc"]
     (case type
       :home               base
       :issues             (str base "/issues")
       :rationale          (str base "#rationale")
       :contributors       (str base "/graphs/contributors")
-      :userguide/articles (str base "/blob/master/doc/userguide/articles.md")
-      :userguide/scm-faq  (str base "/blob/master/doc/userguide/faq.md#how-do-i-set-scm-info-for-my-project"))))
+      :roadmap            (str base "/blob/master/doc/roadmap.adoc")
+      :userguide/scm-faq  (str base "/blob/master/doc/userguide/faq.md#how-do-i-set-scm-info-for-my-project")
+      :userguide/authors  (str base "/blob/master/doc/userguide/for-library-authors.adoc")
+      :userguide/users    (str base "/blob/master/doc/userguide/for-users.md")
+
+      :userguide/basic-setup  (str (github-url :userguide/authors) "#basic-setup")
+      :userguide/articles     (str (github-url :userguide/authors) "#articles")
+      :userguide/offline-docs (str (github-url :userguide/users) "#offline-docs"))))
 
 (defn strip-common-start-string
   "Remove the common substring from `s2` that both, `s1`
