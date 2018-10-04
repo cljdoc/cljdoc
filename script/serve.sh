5d891ab98d4310f39198d8c5861877c50ae2a846
Jorin Vogel <hi@jorin.me>
2018-10-04 23:41:19 +0200

    Fix a typo in script/serve.sh

--- script/serve.sh
+++ script/serve.sh
@@ -2,7 +2,7 @@
 
 set -eo pipefail
 
-# we use http-server from NPN here because python's
+# we use http-server from NPM here because python's
 # SimpleHTTP doesn't like it when the directory it's
 # serving dissapears every now and then
 npx http-server target/grimoire-html/ -c-1 -o
