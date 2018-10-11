cebcbbe5528e935b043883cebadb7f8160a274de
Martin Klepsch <martinklepsch@googlemail.com>
2018-10-11 15:05:19 +0200

    properly set deps for jar files

--- script/build-libs.sh
+++ script/build-libs.sh
@@ -8,9 +8,9 @@ if ! hash boot 2>/dev/null; then
 fi
 
 pushd modules/shared-utils
-boot -d seancorfield/boot-tools-deps deps pom -p cljdoc/shared-utils -v 0.1.0-SNAPSHOT jar install
+boot -d seancorfield/boot-tools-deps deps -B pom -p cljdoc/shared-utils -v 0.1.0-SNAPSHOT jar install
 popd
 
 pushd modules/analysis-runner
-boot -d seancorfield/boot-tools-deps deps pom -p cljdoc/analysis-runner -v 0.1.0-SNAPSHOT jar install
+boot -d seancorfield/boot-tools-deps deps -B pom -p cljdoc/analysis-runner -v 0.1.0-SNAPSHOT jar install
 popd
