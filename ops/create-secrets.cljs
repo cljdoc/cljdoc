ee3f687d129ad6d36987744d470a9db01f76b3cc
Martin Klepsch <martinklepsch@googlemail.com>
2018-10-18 09:45:51 +0200

    clean out secrets script

--- ops/create-secrets.cljs
+++ ops/create-secrets.cljs
@@ -11,13 +11,7 @@
   (get-in tf-outputs [k "value"]))
 
 (defn build-edn [tf-outputs]
-  (->> {:aws {:access-key (tf-val tf-outputs "bucket_user_access_key")
-              :secret-key (tf-val tf-outputs "bucket_user_secret_key")
-              :s3-bucket-name (tf-val tf-outputs "bucket_name")
-              :cloudfront-id (tf-val tf-outputs "cloudfront_id")
-              :cloudfront-url (tf-val tf-outputs "cloudfront_url")
-              :r53-hosted-zone (tf-val tf-outputs "cloudfront_url")}
-        :circle-ci {:api-token js/process.env.CIRCLE_API_TOKEN
+  (->> {:circle-ci {:api-token js/process.env.CIRCLE_API_TOKEN
                     :builder-project js/process.env.CIRCLE_BUILDER_PROJECT}
         :sentry {:dsn js/process.env.SENTRY_DSN}
         :telegram {:bot-token js/process.env.TELEGRAM_BOT_TOKEN
