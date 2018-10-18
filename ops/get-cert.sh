20cc3dc6af4b847f919c274bba45e2c13e0cb0ad
Daniel Compton <desk@danielcompton.net>
2018-10-19 08:29:08 +1300

    Change primary domain from cljdoc.xyz to cljdoc.org
    Fixes #135

--- ops/get-cert.sh
+++ ops/get-cert.sh
@@ -2,13 +2,23 @@
 
 set -eou pipefail
 
-domain="cljdoc.xyz"
+domain="cljdoc.org"
 
-echo "--- Getting Certificate -------------------------------------------------------"
+echo "--- Getting .org Certificate -------------------------------------------------------"
 
 certbot certonly -m martinklepsch@gmail.com -d "$domain" --nginx --agree-tos --non-interactive
 
-echo "--- Linking Server Block ------------------------------------------------------"
+echo "--- Linking .org Server Block ------------------------------------------------------"
+
+ln -s "/etc/nginx/sites-available/$domain.conf" "/etc/nginx/sites-enabled/$domain.conf"
+
+domain="cljdoc.xyz"
+
+echo "--- Getting .xyz Certificate -------------------------------------------------------"
+
+certbot certonly -m martinklepsch@gmail.com -d "$domain" --nginx --agree-tos --non-interactive
+
+echo "--- Linking .xyz Server Block ------------------------------------------------------"
 
 ln -s "/etc/nginx/sites-available/$domain.conf" "/etc/nginx/sites-enabled/$domain.conf"
 
--- ops/public_bucket/bucket.tf
+++ ops/public_bucket/bucket.tf
@@ -27,12 +27,14 @@ resource "aws_s3_bucket" "public_bucket" {
 
 data "aws_iam_policy_document" "bucket_write_user_policy" {
   provider = "aws.prod"
+
   statement {
     effect  = "Allow"
     actions = ["s3:*"]
+
     resources = [
       "${aws_s3_bucket.public_bucket.arn}",
-      "${aws_s3_bucket.public_bucket.arn}/*"
+      "${aws_s3_bucket.public_bucket.arn}/*",
     ]
   }
 }
@@ -47,8 +49,8 @@ resource "aws_iam_user_policy" "bucket_write_user_policy" {
 
 resource "aws_iam_user" "bucket_write_user" {
   provider = "aws.prod"
-  name = "${var.bucket_name}-user"
-  path = "/cljdoc/"
+  name     = "${var.bucket_name}-user"
+  path     = "/cljdoc/"
 }
 
 resource "aws_iam_access_key" "bucket_write_user_key" {
@@ -57,13 +59,13 @@ resource "aws_iam_access_key" "bucket_write_user_key" {
 }
 
 output "bucket_name" {
-    value = "${var.bucket_name}"
+  value = "${var.bucket_name}"
 }
 
 output "write_user_access_key" {
-    value = "${aws_iam_access_key.bucket_write_user_key.id}"
+  value = "${aws_iam_access_key.bucket_write_user_key.id}"
 }
 
 output "write_user_secret" {
-    value = "${aws_iam_access_key.bucket_write_user_key.secret}"
+  value = "${aws_iam_access_key.bucket_write_user_key.secret}"
 }
