f11dcb10d9886c16becdcc688daf4f80653c9f26
Avichal <pandeyavichal7@gmail.com>
2018-10-20 19:22:20 +0200

    Fix tests

--- test/cljdoc/util_test.clj
+++ test/cljdoc/util_test.clj
@@ -64,10 +64,10 @@
     (t/is (= (str {:or #"^Test*"}) (str (util/read-cljdoc-edn (StringReader. "{:or #regex \"^Test*\"}"))))))
 
 (t/deftest day-suffix-test
-  (t/is (= "st" (dt/date-suffix 1)))
-  (t/is (= "nd" (dt/date-suffix 2)))
-  (t/is (= "rd" (dt/date-suffix 3)))
-  (t/is (= "th" (dt/date-suffix 15))))
+  (t/is (= "st" (dt/day-suffix 1)))
+  (t/is (= "nd" (dt/day-suffix 2)))
+  (t/is (= "rd" (dt/day-suffix 3)))
+  (t/is (= "th" (dt/day-suffix 15))))
 
 (t/deftest analytics-format-test
   (t/is (= "Wed, Oct 17th" (dt/->analytics-format "2018-10-17T20:58:21.491730Z"))))
