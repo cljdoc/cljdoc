77c8a8c4daa9640bb9fcf76e146bf4d58b36f8ca
Daw-Ran Liou <dawran6@gmail.com>
2018-10-31 19:10:13 -0700

    Change to current versioned URL

--- src/cljdoc/server/sitemap.clj
+++ src/cljdoc/server/sitemap.clj
@@ -8,10 +8,11 @@
 
 (defn sitemap [db-spec]
   (generate-sitemap
-   (for [{:keys [group_id artifact_id] :as doc} (storage/all-distinct-docs db-spec)]
+   (for [{:keys [group_id artifact_id name] :as doc} (storage/all-distinct-docs db-spec)]
      {:loc
-      (routes/url-for :artifact/current :path-params {:group-id group_id
-                                                      :artifact-id artifact_id})}))
+      (routes/url-for :artifact/version :path-params {:group-id group_id
+                                                      :artifact-id artifact_id
+                                                      :version name})}))
   )
 
 (comment
@@ -20,7 +21,7 @@
         (cfg/db)
         (storage/->SQLiteStorage)))
 
-  (routes/url-for :artifact/current :path-params {:group-id "a" :artifact-id "b"})
+  (routes/url-for :artifact/version :path-params {:group-id "a" :artifact-id "b" :version "c"})
 
   (storage/all-distinct-docs db-spec)
 
