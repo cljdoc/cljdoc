4b3b86c510bf7708c154902e98e6c417e97d0713
Martin Klepsch <martinklepsch@googlemail.com>
2018-11-04 13:24:04 +0100

    update stats.sh script

--- ops/stats.sh
+++ ops/stats.sh
@@ -2,7 +2,7 @@
 
 set -eou pipefail
 
-ssh root@cljdoc.xyz cat /var/log/nginx/cljdoc-access.log \
+ssh root@cljdoc.org cat /var/log/nginx/cljdoc-access.log \
     | goaccess -c \
                --log-format='%h %^[%d:%t %^] "%r" %s %b "%R" "%u" %^ %T' \
                --date-format '%d/%b/%Y' \
