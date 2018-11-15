ddb18b76f86fa3a7cceb809ead13315f2d9eb909
Martin Klepsch <martinklepsch@googlemail.com>
2018-11-16 00:41:12 +0100

    add testing script with some cases and explanations

---
+++ modules/analysis-runner/extended-test.clj
@@ -0,0 +1,41 @@
+(require '[clojure.java.shell :as sh]
+         '[clojure.string :as string]
+         '[clojure.pprint])
+
+(def candidates
+  [;; depends on ring/ring-core which requires [javax.servlet/servlet-api "2.5"]
+   ["metosin/compojure-api" "2.0.0-alpha27" "http://repo.clojars.org/metosin/compojure-api/2.0.0-alpha27/compojure-api-2.0.0-alpha27"]
+   ;; depends on tools.namepspace 0.3.0-alpha4, cljdoc explicitly declared 0.2.11
+   ["iced-nrepl" "0.2.5" "https://repo.clojars.org/iced-nrepl/iced-nrepl/0.2.5/iced-nrepl-0.2.5"]
+   ;; known to work
+   ["bidi" "2.1.3" "http://repo.clojars.org/bidi/bidi/2.1.3/bidi-2.1.3"]
+   ;; had some issues with older ClojureScript and analysis env
+   ["orchestra" "2018.11.07-1" "http://repo.clojars.org/orchestra/orchestra/2018.11.07-1/orchestra-2018.11.07-1"]
+   ;; might have had some issues related to old versions of core.async in the past
+   ["manifold" "0.1.8" "http://repo.clojars.org/manifold/manifold/0.1.8/manifold-0.1.8"]])
+
+(def sep
+  "\n---------------------------------------------------------------------------")
+
+(defn exit [results]
+  (clojure.pprint/print-table results)
+  (shutdown-agents)
+  (if (some false? (map :success? results))
+    (System/exit 1)
+    (System/exit 0)))
+
+(->> (for [[project version url-base] candidates]
+       (let [args ["clojure" "-m" "cljdoc.analysis.runner" project version (str url-base ".jar") (str url-base ".pom")]
+             _ (do (println "Analyzing" project version) (println (string/join " " args) sep))
+             {:keys [exit out]} (apply sh/sh args)]
+         (when-not (zero? exit)
+           (println "Analysis failed for" project version sep)
+           (println out)
+           (println sep))
+
+         {:project project
+          :version version
+          :success? (zero? exit)}))
+     (sort-by :success?)
+     (exit))
+
