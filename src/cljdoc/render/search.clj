67f0c80307b1e855c5a5df5e7d4d7b35f7ad8bba
Vincent Cantin <vincent.cantin@le-moulin-studio.com>
2018-11-19 11:13:17 +0800

    Added a navigation bar at the top of the search page which let the user go back to the home page.

--- src/cljdoc/render/search.clj
+++ src/cljdoc/render/search.clj
@@ -10,13 +10,15 @@
 
 
 (defn search-page [context]
-  (->> [:div.pt4
-        [:div.mt5-ns.mw7.center.pa4.pa0-l
-         [:h1.ma0
-          [:span.dn "cljdoc beta"]
-          [:img {:src "/cljdoc-logo-beta.svg" :alt "cljdoc logo" :width "150px"}]]
-         [:p.f2-ns.f3.mv3.w-90-l.lh-copy "Library search:"]
-         (search-form (-> context :request :query-params :q))]]
+  (->> [:div
+        (layout/top-bar-generic)
+        [:div.pt4
+         [:div.mt5-ns.mw7.center.pa4.pa0-l
+          [:h1.ma0
+           [:span.dn "cljdoc beta"]
+           [:img {:src "/cljdoc-logo-beta.svg" :alt "cljdoc logo" :width "150px"}]]
+          [:p.f2-ns.f3.mv3.w-90-l.lh-copy "Library search:"]
+          (search-form (-> context :request :query-params :q))]]]
        (layout/page {:title "cljdoc — documentation for Clojure/Script libraries"
                      :responsive? true})
        (str)))
