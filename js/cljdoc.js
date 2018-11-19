cljdoc/cljdoc

commit 60aa57e401a02fe8f6f92f645c5de1d9e24ccd13

fix #156: responsify all the things

js/cljdoc.js
@@ -8,8 +8,10 @@ function isProjectDocumentationPage() {
 }
 
 function initSrollIndicator() {
-  var mainScrollView = document.querySelector(".main-scroll-view");
-  var sidebarScrollView = document.querySelector(".sidebar-scroll-view");
+  var mainScrollView = document.querySelector(".js--main-scroll-view");
+  var sidebarScrollView = document.querySelector(
+    ".js--namespace-contents-scroll-view"
+  );
   var defBlockTitles = Array.from(
     document.querySelectorAll(".def-block-title")
   );
@@ -80,7 +82,7 @@ function restoreSidebarScrollPos() {
     .join("/");
 
   if (scrollPosData && page == scrollPosData.page) {
-    Array.from(document.querySelectorAll(".js--sidebar"))[0].scrollTop =
+    Array.from(document.querySelectorAll(".js--main-sidebar"))[0].scrollTop =
       scrollPosData.scrollTop;
   }
 

src/cljdoc/render/layout.clj
@@ -41,6 +41,7 @@
                [:html {}
                 [:head
                  [:title (:title opts)]
+                 [:meta {:charset "utf-8"}]
                  [:meta {:content (:description opts) :name "description"}]
 
                  ;; Google / Search Engine Tags
@@ -60,9 +61,7 @@
                    (assert (.startsWith url "/"))
                    [:link {:rel "canonical" :href (str "https://cljdoc.org" url)}]); TODO read domain from config
 
-                 (when (:responsive? opts)
-                   [:meta {:name "viewport" :content "width=device-width, initial-scale=1"}])
-                 [:meta {:charset "utf-8"}]
+                 [:meta {:name "viewport" :content "width=device-width, initial-scale=1"}]
                  (hiccup.page/include-css
                    "https://cdn.jsdelivr.net/gh/highlightjs/cdn-release@9.12.0/build/styles/github-gist.min.css"
                    "https://unpkg.com/tachyons@4.9.0/css/tachyons.min.css"
@@ -78,19 +77,6 @@
 
 (defn sidebar-title [title]
   [:h4.ttu.f7.fw5.mt1.mb2.tracked.gray title])
-
-(def TOP-BAR-HEIGHT 57)
-
-(defn sidebar [& contents]
-  [:div.absolute.w5.bottom-0.left-0.pa3.pa4-ns.overflow-scroll.br.b--black-10
-   {:class "js--sidebar"
-    :style {:top (str TOP-BAR-HEIGHT "px")}} ; CSS HACK
-   contents])
-
-(defn sidebar-two [& contents]
-  [:div.absolute.w5.bottom-0.left-0.pa3.pa4-ns.overflow-scroll.br.b--black-10.sidebar-scroll-view
-   {:style {:top (str TOP-BAR-HEIGHT "px") :left "16rem"}} ; CSS HACK
-   contents])
 
 (defn meta-info-dialog []
   [:div#js--meta-dialog.ma3.pa3.ba.br3.b--blue.bw2.w-20.fixed.right-0.bottom-0.bg-white.dn
@@ -109,16 +95,6 @@
    [:a#js--meta-close.link.black.fr.pointer
     "× close"]])
 
-(defn main-container [{:keys [offset extra-height]} & content]
-   [:div.absolute.bottom-0.right-0
-    {:style {:left offset
-             :top (str (+ TOP-BAR-HEIGHT (or extra-height 0)) "px")}}
-    (into [:div.absolute.top-0.bottom-0.left-0.right-0.overflow-y-scroll.ph4-ns.ph2.main-scroll-view
-           [:img#js--meta-icon.ma3.fixed.right-0.bottom-0.bg-white.dn.db-ns.pointer
-            {:src "https://icon.now.sh/explore/48/357edd"}]
-           (meta-info-dialog)]
-          content)])
-
 (defn top-bar-generic []
   [:nav.pv2.ph3.pv3-ns.ph4-ns.bb.b--black-10.flex.items-center
    [:a {:href "/"}
@@ -128,7 +104,7 @@
     "Have Feedback?"]])
 
 (defn top-bar [cache-id scm-url]
-  [:nav.pv2.ph3.pv3-ns.ph4-ns.bb.b--black-10.flex.items-center
+  [:nav.pv2.ph3.pv3-ns.ph4-ns.bb.b--black-10.flex.items-center.bg-white
    [:a.dib.v-mid.link.dim.black.b.f6.mr3 {:href (routes/url-for :artifact/version :path-params cache-id)}
     (util/clojars-id cache-id)]
    [:a.dib.v-mid.link.dim.gray.f6.mr3
@@ -156,3 +132,72 @@
   [:a.link {:href (routes/url-for :artifact/version :path-params version-map)}
    [:div.bg-washed-yellow.pa2.f7.mb4.dark-gray.lh-title
     "A newer version " [:span.blue "(" version ")"] " for this library is available"]])
+
+;; Responsive Layout -----------------------------------------------------------
+
+(def r-main-container
+  "Everything that's rendered on cljdoc goes into this container.
+
+  On desktop screens it fills the viewport forcing child nodes
+  to scroll. On smaller screens it grows with the content allowing
+  Safari to properly condense the URL bar when scrolling.
+
+  In contrast if the container was fixed on mobile as well Safari
+  would perceive the scroll events as if the user scrolled in a
+  smaller portion of the UI.
+
+  While `height: 100vh` would also work on desktop screens there are some
+  known issues around [using viewport height units on mobile](https://nicolas-hoizey.com/2015/02/viewport-height-is-taller-than-the-visible-part-of-the-document-in-some-mobile-browsers.html)."
+  :div.flex.flex-column.fixed-ns.bottom-0.left-0.right-0.top-0)
+
+(def r-sidebar-container
+  :nav.js--main-sidebar.w5.pa3.pa4-ns.br.b--black-10.db-ns.dn.overflow-y-scroll.border-box.flex-shrink-0)
+
+(def r-api-sidebar-container
+  :div.js--namespace-contents-scroll-view.w5.pa3.pa4-ns.br.b--black-10.db-ns.dn.overflow-y-scroll.flex-shrink-0)
+
+(def r-content-container
+  :div.js--main-scroll-view.ph4-ns.ph3.flex-grow-1.overflow-y-scroll)
+
+(def r-top-bar-container
+  "Additional wrapping to make the top bar responsive
+
+  On small screens we render the top bar and mobile navigation as a
+  `fixed` div so that the main container can be scrolled without the
+  navigation being scrolled out of view.
+
+  On regular sized screens we use the default positioning setting of
+  `static` so that the top bar is rendered as a row of the main
+  container.
+
+  The z-index `z3` setting is necessary to ensure `fixed` elements
+  appear above other elements"
+  :div.fixed.top-0.left-0.right-0.static-ns.flex-shrink-0.z3)
+
+(def mobile-nav-spacer
+  "Spacer so fixed navigation elements don't overlap content
+  This is only needed on small screens so `dn-ns` hides it
+  on non-small screens
+
+  The height has been found by trial and error."
+  [:div.bg-blue.dn-ns.pt4.tc
+   {:style {:height "5.2rem"}}
+   ;; TODO render different fun messages for each request
+   [:span.b.white.mt3 "Liking cljdoc? Tell your friends :D"]])
+
+(defn layout
+  [{:keys [top-bar
+           main-sidebar-contents
+           vars-sidebar-contents
+           content]}]
+  [r-main-container
+   [r-top-bar-container
+    top-bar
+    [:div#js--mobile-nav.db.dn-ns]]
+   mobile-nav-spacer
+   [:div.flex.flex-row
+    (into [r-sidebar-container] main-sidebar-contents)
+    (when (seq vars-sidebar-contents)
+      (into [r-api-sidebar-container] vars-sidebar-contents))
+    ;; (when doc-html doc-nav)
+    [r-content-container content]]])

js/index.js
@@ -2,6 +2,7 @@ import { render, h } from "preact";
 import { trackProjectOpened, Switcher } from "./switcher";
 import { hideNestedArticles } from "./doctree";
 import { App } from "./search";
+import { MobileNav } from "./mobile";
 import {
   isNSPage,
   isProjectDocumentationPage,
@@ -24,6 +25,7 @@ if (isNSPage()) {
 }
 
 if (isProjectDocumentationPage()) {
+  render(h(MobileNav), document.querySelector("#js--mobile-nav"));
   toggleMetaDialog();
 }
 

js/mobile.js
@@ -1,0 +1,46 @@
+import { Component, render, h } from "preact";
+import * as doctree from "./doctree";
+
+export class MobileNav extends Component {
+  constructor() {
+    super();
+    this.toggleNav = this.toggleNav.bind(this);
+  }
+
+  toggleNav() {
+    let mainScrollView = document.querySelector(".js--main-scroll-view");
+    let mainSidebar = document.querySelector(".js--main-sidebar");
+    let isMainContentHidden = mainScrollView.classList.contains("dn");
+    if (isMainContentHidden) {
+      mainScrollView.classList.remove("dn"); // show main scroll view / content area
+      mainSidebar.classList.replace("db", "dn"); // hide sidebar
+      doctree.hideNestedArticles();
+      this.setState({ showNav: false });
+    } else {
+      mainScrollView.classList.add("dn"); // hide main scroll view / content area
+      mainSidebar.classList.add("flex-grow-1"); // make sure nav fills width of screen
+      mainSidebar.classList.replace("dn", "db"); // show sidebar
+      doctree.showNestedArticles();
+      this.setState({ showNav: true });
+    }
+  }
+
+  render(props, state) {
+    let btnMsg = state.showNav
+      ? "Back to Content"
+      : "Tap for Articles & Namespaces";
+    let btnIcon = state.showNav ? "chevronLeft" : "list";
+    let btnSrc = "https://icon.now.sh/" + btnIcon + "/32";
+    return (
+      <div class="bg-light-gray">
+        <button
+          class="outline-0 bw0 bg-transparent w-100 tl pa2"
+          onClick={this.toggleNav}
+        >
+          <img class="dib mr2 v-mid" src={btnSrc} height="32" />
+          <span class="dib">{btnMsg}</span>
+        </button>
+      </div>
+    );
+  }
+}

src/cljdoc/render/api.clj
@@ -201,20 +201,17 @@
 
 (defn sub-namespace-overview-page
   [{:keys [ns-entity namespaces defs top-bar-component article-list-component namespace-list-component]}]
-  [:div.ns-page
-   top-bar-component
-   (layout/sidebar
-    article-list-component
-    namespace-list-component)
-   (layout/main-container
-    {:offset "16rem"}
-    [:div.w-80-ns.pv5
-     (for [mp-ns (->> namespaces
-                     (filter #(.startsWith (platf/get-field % :name) (:namespace ns-entity))))
-           :let [ns (platf/get-field mp-ns :name)
-                 ns-url-fn #(routes/url-for :artifact/namespace :path-params (assoc ns-entity :namespace %))
-                 defs (bundle/defs-for-ns defs ns)]]
-       (namespace-overview ns-url-fn mp-ns defs))])])
+  (layout/layout
+   {:top-bar top-bar-component
+    :main-sidebar-contents [article-list-component
+                            namespace-list-component]
+    :content [:div.mw7.center.pv4
+              (for [mp-ns (->> namespaces
+                               (filter #(.startsWith (platf/get-field % :name) (:namespace ns-entity))))
+                    :let [ns (platf/get-field mp-ns :name)
+                          ns-url-fn #(routes/url-for :artifact/namespace :path-params (assoc ns-entity :namespace %))
+                          defs (bundle/defs-for-ns defs ns)]]
+                (namespace-overview ns-url-fn mp-ns defs))]}))
 
 (defn add-src-uri
   [{:keys [platforms] :as mp-var} scm-base file-mapping]
@@ -239,24 +236,21 @@
         render-wiki-link (render-wiki-link-fn (:namespace ns-entity)
                                               #(routes/url-for :artifact/namespace :path-params (assoc ns-entity :namespace %)))]
     [:div.ns-page
-     top-bar-component
-     (layout/sidebar
-      upgrade-notice-component
-      article-list-component
-      namespace-list-component)
-     (layout/sidebar-two
-      (platform-support-note platf-stats)
-      (definitions-list ns-entity defs
-        {:indicate-platforms-other-than dominant-platf}))
-     (layout/main-container
-      {:offset "32rem"}
-      [:div.w-80-ns.pv4
-       [:h2 (:namespace ns-entity)]
-       (render-doc ns-data render-wiki-link)
-       (for [def defs]
-         (def-block
-           (add-src-uri def scm-base file-mapping)
-           render-wiki-link))])]))
+     (layout/layout
+      {:top-bar top-bar-component
+       :main-sidebar-contents [upgrade-notice-component
+                               article-list-component
+                               namespace-list-component]
+       :vars-sidebar-contents [(platform-support-note platf-stats)
+                               (definitions-list ns-entity defs
+                                 {:indicate-platforms-other-than dominant-platf})]
+       :content [:div.w-80-ns.pv4
+                 [:h2 (:namespace ns-entity)]
+                 (render-doc ns-data render-wiki-link)
+                 (for [def defs]
+                   (def-block
+                     (add-src-uri def scm-base file-mapping)
+                     render-wiki-link))]})]))
 
 (comment
   (:platforms --d)

src/cljdoc/render/articles.clj
@@ -49,44 +49,38 @@
                         namespace-list-component
                         doc-scm-url
                         doc-html] :as args}]
-  [:div
-   top-bar-component
-   (layout/sidebar
-    upgrade-notice-component
-    (article-list doc-tree-component)
-    namespace-list-component)
-   (layout/main-container
-    (cond-> {:offset "16rem"}
-      doc-html (assoc :extra-height 34))
-    [:div.mw7.center
-     ;; TODO dispatch on a type parameter that becomes part of the attrs map
-     (if doc-html
-       [:div#doc-html.markdown.lh-copy.pv4
-        (hiccup/raw doc-html)
-        [:a.db.f7.tr {:href doc-scm-url} (if (= :gitlab (util/scm-provider doc-scm-url))
-                                           "Edit on GitLab"
-                                           "Edit on GitHub")]]
-       [:div.lh-copy.pv6.tc
-        #_[:pre (pr-str (dissoc args :top-bar-component :doc-tree-component :namespace-list-component))]
-        [:span.f4.serif.gray.i "Space intentionally left blank."]])])])
+  (layout/layout
+   {:top-bar top-bar-component
+    :main-sidebar-contents [upgrade-notice-component
+                            (article-list doc-tree-component)
+                            namespace-list-component]
+    :content [:div.mw7.center
+              ;; TODO dispatch on a type parameter that becomes part of the attrs map
+              (if doc-html
+                [:div#doc-html.markdown.lh-copy.pv4
+                 (hiccup/raw doc-html)
+                 [:a.db.f7.tr {:href doc-scm-url} (if (= :gitlab (util/scm-provider doc-scm-url))
+                                                    "Edit on GitLab"
+                                                    "Edit on GitHub")]]
+                [:div.lh-copy.pv6.tc
+                 #_[:pre (pr-str (dissoc args :top-bar-component :doc-tree-component :namespace-list-component))]
+                 [:span.f4.serif.gray.i "Space intentionally left blank."]])]}))
 
 (defn doc-overview [{:keys [top-bar-component
                             doc-tree-component
                             namespace-list-component
                             cache-id
                             doc-tree] :as args}]
-  [:div
-   top-bar-component
-   (layout/sidebar
-    (article-list doc-tree-component)
-    namespace-list-component)
-   (layout/main-container
-    {:offset "16rem"}
-    [:div.mw7.center.pv4
-     [:h1 (:title doc-tree)]
-     [:ol
-      (for [c (:children doc-tree)]
-        [:li.mv2
-         [:a.link.blue
-          {:href (doc-link cache-id (-> c :attrs :slug-path))}
-          (-> c :title)]])]])])
+  [:div.doc-page
+   (layout/layout
+    {:top-bar top-bar-component
+     :main-sidebar-contents [(article-list doc-tree-component)
+                             namespace-list-component]
+     :content [:div.mw7.center.pv4
+               [:h1 (:title doc-tree)]
+               [:ol
+                (for [c (:children doc-tree)]
+                  [:li.mv2
+                   [:a.link.blue
+                    {:href (doc-link cache-id (-> c :attrs :slug-path))}
+                    (-> c :title)]])]]})])

src/cljdoc/renderers/html.clj
@@ -96,12 +96,15 @@
 
 (defmethod render :artifact/version
   [_ route-params {:keys [cache-id cache-contents] :as cache-bundle}]
-  (->> [:div
-        (layout/top-bar cache-id (-> cache-contents :version :scm :url))
-        (layout/sidebar
-         (articles/article-list
-          (articles/doc-tree-view cache-id (doctree/add-slug-path (-> cache-contents :version :doc)) []))
-         (api/namespace-list {} (bundle/ns-entities cache-bundle)))]
+  (->> (layout/layout
+        ;; TODO on mobile this will effectively be rendered as a blank page
+        ;; We could instead show a message and the namespace tree.
+        {:top-bar (layout/top-bar cache-id (-> cache-contents :version :scm :url))
+         :main-sidebar-contents [(articles/article-list
+                                  (articles/doc-tree-view cache-id
+                                                          (doctree/add-slug-path (-> cache-contents :version :doc))
+                                                          []))
+                                 (api/namespace-list {} (bundle/ns-entities cache-bundle))]})
        (layout/page {:title (str (util/clojars-id cache-id) " " (:version cache-id))
                      :description (layout/description cache-id)})))
 

commit 4dd852670d38ab27910b1920fb4c1a59d0e46872

fix meta info dialog (broke it in #211)

js/cljdoc.js
@@ -90,7 +90,7 @@ function restoreSidebarScrollPos() {
 }
 
 function toggleMetaDialog() {
-  if (document.querySelector(".main-scroll-view")) {
+  if (document.querySelector(".js--main-scroll-view")) {
     document.getElementById("js--meta-icon").onclick = function() {
       document.getElementById("js--meta-icon").classList.replace("db-ns", "dn");
       document

src/cljdoc/render/layout.clj
@@ -82,21 +82,24 @@
   [:h4.ttu.f7.fw5.mt1.mb2.tracked.gray title])
 
 (defn meta-info-dialog []
-  [:div#js--meta-dialog.ma3.pa3.ba.br3.b--blue.bw2.w-20.fixed.right-0.bottom-0.bg-white.dn
-   [:p.ma0
-    [:b "cljdoc"]
-    " is a website building & hosting documentation for Clojure/Script libraries"]
-   (into [:div.mv3]
-         (map (fn [[description link]]
-                [:a.db.link.black.mv1.pv3.tc.br2.pointer
-                 {:href link, :style {:background-color "#ECF2FB"}}
-                 description])
-              [["Keyboard shortcuts"  (routes/url-for :shortcuts)]
-               ["Report a problem"    (util/github-url :issues)]
-               ;; ["Recent improvements" "#"] TODO add link once it exists
-               ["cljdoc on GitHub"    (util/github-url :home)]]))
-   [:a#js--meta-close.link.black.fr.pointer
-    "× close"]])
+  [:div
+   [:img#js--meta-icon.ma3.fixed.right-0.bottom-0.bg-white.dn.db-ns.pointer
+    {:src "https://icon.now.sh/explore/48/357edd"}]
+   [:div#js--meta-dialog.ma3.pa3.ba.br3.b--blue.bw2.w-20.fixed.right-0.bottom-0.bg-white.dn
+    [:p.ma0
+     [:b "cljdoc"]
+     " is a website building & hosting documentation for Clojure/Script libraries"]
+    (into [:div.mv3]
+          (map (fn [[description link]]
+                 [:a.db.link.black.mv1.pv3.tc.br2.pointer
+                  {:href link, :style {:background-color "#ECF2FB"}}
+                  description])
+               [["Keyboard shortcuts"  (routes/url-for :shortcuts)]
+                ["Report a problem"    (util/github-url :issues)]
+                ;; ["Recent improvements" "#"] TODO add link once it exists
+                ["cljdoc on GitHub"    (util/github-url :home)]]))
+    [:a#js--meta-close.link.black.fr.pointer
+     "× close"]]])
 
 (defn top-bar-generic []
   [:nav.pv2.ph3.pv3-ns.ph4-ns.bb.b--black-10.flex.items-center
@@ -207,4 +210,5 @@
     (when (seq vars-sidebar-contents)
       (into [r-api-sidebar-container] vars-sidebar-contents))
     ;; (when doc-html doc-nav)
-    [r-content-container content]]])
+    [r-content-container content]
+    (meta-info-dialog)]])