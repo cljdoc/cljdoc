ee613cd4b57a192ad9db4c7cf347298a06beff7b
Daw-Ran Liou <dawran6@gmail.com>
2018-11-19 08:22:30 -0800

    Add explicit 404 message on not-found page

--- src/cljdoc/render/error.clj
+++ src/cljdoc/render/error.clj
@@ -7,7 +7,8 @@
 (defn not-found-404 []
   (->> [:div.pt4
         [:div.mt5-ns.mt6-l.mw7.center.pa4.pa0-l
-         [:p.f2-ns.f3.mv3.w-90-l.lh-copy "We couldn't find anything here..."]
+         [:h1.f2.f1-ns.b ":("]
+         [:h2.f3.f2-ns.ttu "404 - Page Not Found"]
          [:p.lh-copy.i "What library are you looking for? Find it here 👇"]
          (home/search-app)]]
        (layout/page {})
