f36054a1b52cfcfd1271783155b5dadd37b22564
Daw-Ran Liou <dawran6@gmail.com>
2018-11-21 13:26:55 -0800

    Split Readme and Changelog from the rest of article list

--- src/cljdoc/renderers/html.clj
+++ src/cljdoc/renderers/html.clj
@@ -40,6 +40,9 @@
   (assert (:doc-slug-path route-params))
   (let [doc-slug-path (:doc-slug-path route-params)
         doc-tree (doctree/add-slug-path (-> cache-contents :version :doc))
+        [doc-tree-with-readme-and-changelog doc-tree-with-rest] (partition-by (fn is-readme-or-changelog [entry]
+                                                                                (contains? #{"Readme" "Changelog"} (:title entry)))
+                                                                              doc-tree)
         doc-p (->> doc-tree
                    doctree/flatten*
                    (filter #(= doc-slug-path (:slug-path (:attrs %))))
@@ -47,7 +50,9 @@
         doc-html (or (some-> doc-p :attrs :cljdoc/markdown rich-text/markdown-to-html)
                      (some-> doc-p :attrs :cljdoc/asciidoc rich-text/asciidoc-to-html))
         common {:top-bar-component (layout/top-bar cache-id (-> cache-contents :version :scm :url))
-                :doc-tree-component (articles/doc-tree-view cache-id doc-tree doc-slug-path)
+                :doc-tree-component [:div
+                                     (articles/doc-tree-view cache-id doc-tree-with-readme-and-changelog doc-slug-path)
+                                     (articles/doc-tree-view cache-id doc-tree-with-rest doc-slug-path)]
                 :namespace-list-component (api/namespace-list {} (bundle/ns-entities cache-bundle))
                 :upgrade-notice-component (if-let [newer-v (bundle/more-recent-version cache-bundle)]
                                             (layout/upgrade-notice newer-v))}]
