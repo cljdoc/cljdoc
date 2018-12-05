f76594f0067c3b30f6ada66bef12883d7b488588
Martin Klepsch <martinklepsch@googlemail.com>
2018-12-05 11:49:05 +0100

    add more comments to ops/nomad.sh

--- ops/nomad.sh
+++ ops/nomad.sh
@@ -1,10 +1,17 @@
 #!/bin/bash
 
-# This is largely inspired by http://mpharrigan.com/2016/05/17/background-ssh.html
+# WHAT
+# This script can be used to launch an SSH process forwarding nomad and consul ports
+# to localhost. The script also launches a subshell with an adjusted `nomad>` prompt.
+# The port forwarding is terminated when the subshell exits.
+# This allows users to communicate with the Nomad server using their local `nomad`
+# binary. Ex.: `nomad status cljdoc`, `nomad alloc logs -f $alloc_id`, and so on.
+
+# HOW
+# It is largely inspired by http://mpharrigan.com/2016/05/17/background-ssh.html
 # An SSH process is launched to forward Nomad and Consul ports and backgrounded
 # That process uses a tmp-file as socket which allows us to later terminate the
 # forwarding by via `-O exit`.
-
 # To ensure the SSH process is always terminated `trap` is used to run `-O exit`
 # when the process exits, fails or is interrupted
 
