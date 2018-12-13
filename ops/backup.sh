92dd30e5a460f192e9e883dd2905541cadc81e59
Martin Klepsch <martinklepsch@googlemail.com>
2018-12-13 22:12:39 +0100

    update backup script for new ops2.0 setup

--- ops/backup.sh
+++ ops/backup.sh
@@ -2,19 +2,22 @@
 
 set -eou pipefail
 
+git_root=$(git rev-parse --show-toplevel)
+local_backups="$git_root/ops/prod-backup"
+
 tf_out () {
-  git_root=$(git rev-parse --show-toplevel)
   terraform output -state="$git_root/ops/infrastructure/terraform.tfstate" $1
 }
 
-ip=$(tf_out api_ip)
+ip=$(tf_out main_ip)
 date=$(date "+%Y-%m-%d")
 tar_file="backup-$date.tar.gz"
 
 
+ssh root@$ip rm -rf /tmp/cljdoc-backup/
 ssh root@$ip mkdir /tmp/cljdoc-backup/
 
-ssh root@$ip sqlite3 /var/cljdoc/cljdoc.db.sqlite \".backup '/tmp/cljdoc-backup/cljdoc.db.sqlite'\"
+ssh root@$ip sqlite3 /data/cljdoc/cljdoc.db.sqlite \".backup '/tmp/cljdoc-backup/cljdoc.db.sqlite'\"
 
 ssh root@$ip tar -cazf "$tar_file" -C /tmp/cljdoc-backup/ .
 
@@ -22,7 +25,7 @@ entry_count=$(ssh root@$ip tar -tf "$tar_file" | wc -l)
 
 echo "Stored $entry_count files in $tar_file"
 
-scp root@$ip:"$tar_file" prod-backup/
+scp root@$ip:"$tar_file" "$local_backups"
 ssh root@$ip rm "$tar_file"
 ssh root@$ip rm -rf /tmp/cljdoc-backup
 
@@ -30,4 +33,4 @@ echo "Syncing to S3"
 
 AWS_ACCESS_KEY_ID=$(tf_out backups_bucket_user_access_key) \
   AWS_SECRET_ACCESS_KEY=$(tf_out backups_bucket_user_secret_key) \
-  aws s3 sync prod-backup/ s3://$(tf_out backups_bucket_name)/
+  aws s3 sync "$local_backups" s3://$(tf_out backups_bucket_name)/
