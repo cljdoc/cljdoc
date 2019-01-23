59d4cf1cacf4c338f9794e829cac913e3d2ecd2b
Martin Klepsch <martinklepsch@googlemail.com>
2019-01-23 13:44:55 +0100

    add helpers to convert SCM URIs between HTTP<>SSH

--- test/cljdoc/util/scm_test.clj
+++ test/cljdoc/util/scm_test.clj
@@ -19,3 +19,13 @@
   (t/is (= :gitlab (scm/provider "https://gitlab.com/eval/otarta")))
   (t/is (= nil (scm/provider "https://gitea.heevyis.ninja/josha/formulare")))
   (t/is (= nil (scm/provider "https://unknown-scm.com/circleci/clj-yaml"))))
+
+(t/deftest scm-uri-inversion-test-to-ssh
+  (t/is (= "git@github.com:circleci/clj-yaml.git" (scm/ssh-uri "https://github.com/circleci/clj-yaml")))
+  (t/is (= "git@gitea.heevyis.ninja:josha/formulare.git" (scm/ssh-uri "https://gitea.heevyis.ninja/josha/formulare")))
+  (t/is (= "git@unknown-scm.com:circleci/clj-yaml.git" (scm/ssh-uri "https://unknown-scm.com/circleci/clj-yaml"))))
+
+(t/deftest scm-uri-inversion-test-to-http
+  (t/is (= "http://github.com/circleci/clj-yaml" (scm/http-uri "git@github.com:circleci/clj-yaml.git")))
+  (t/is (= "http://gitea.heevyis.ninja/josha/formulare" (scm/http-uri "git@gitea.heevyis.ninja:josha/formulare.git")))
+  (t/is (= "http://unknown-scm.com/circleci/clj-yaml" (scm/http-uri "git@unknown-scm.com:circleci/clj-yaml"))))
