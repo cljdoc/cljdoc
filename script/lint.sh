57bd97cb0b3ffe0f7eaeb05baf14f2c777f4495e
lread <lee@dlread.com>
2019-07-29 14:32:26 -0400

    add clj-kondo linting
    incorporate feedback from review with @borkdude.
    - don't need to build cache then lint, can build cache and lint
    - include test paths when building cache
    - decided to listen to :not-empty? warning after all

--- script/lint.sh
+++ script/lint.sh
@@ -1,11 +1,13 @@
 #!/usr/bin/env bash
 
 #
-# Temporary lint script
 # We can turf this script (or at least get more raw) when either of the following is true:
 # 1) We decide to stop employing unused destructured keys as documentation
 # 2) clj-kondo supports ignoring this specific use of unused bindings
 #
+# Reasonable shortcut:
+# cljdocs has a few small sub-projects under modules, we could lint those separately but
+# instead we are linting all cljdoc source at once.
 
 set -eou pipefail
 
@@ -30,9 +32,20 @@ function is_unused_destructured_key_warning() {
 
 function lint() {
     local out_file=$1
-    shift
+    local lint_args
+    if [ ! -d .clj-kondo/.cache ]; then
+        echo "--[linting and building cache]--"
+        # classpath with tests paths
+        local classpath="$(clojure -R:test -C:test -Spath)"
+        # include modules - but exclude shared-utils as it is already included in deps.edn
+        local modules_paths=$(find modules -name "*" -depth 1 | grep -v "shared-utils")
+        lint_args="$classpath $modules_paths --cache"
+    else
+        echo "--[linting]--"
+        lint_args="src test modules"
+    fi
     set +e
-    clojure -A:clj-kondo --lint $@ &> ${out_file}
+    clojure -A:clj-kondo --lint ${lint_args} &> ${out_file}
     local exit_code=$?
     set -e
     if [ ${exit_code} -ne 0 ] && [ ${exit_code} -ne 2 ] && [ ${exit_code} -ne 3 ]; then
@@ -43,23 +56,15 @@ function lint() {
 }
 
 SCRATCH_DIR=$(mktemp -d -t clj-kondo.out.XXXXXXXXXX)
-
 function cleanup() {
     rm -rf $SCRATCH_DIR
 }
-
 trap cleanup EXIT
 
 EXIT_CODE=0
 SUPPRESSED_COUNT=0
 
-if [ ! -d .clj-kondo/.cache ]; then
-    echo "--[initializing clj-kondo cache]--"
-    lint $SCRATCH_DIR/cache.out $(clojure -Spath) --cache
-fi
-
-echo "--[linting]--"
-lint $SCRATCH_DIR/lint.out src test modules
+lint $SCRATCH_DIR/lint.out
 while read lint_line; do
     if [[ $lint_line =~ ^linting[[:space:]]took ]]; then
         echo $lint_line
