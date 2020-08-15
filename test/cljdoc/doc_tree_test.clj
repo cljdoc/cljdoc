c9384fa739e535724b7de4aa328d5dff91cb8284
Dmytro Palamarchuk <dmytro.p@attendify.com>
2020-08-15 23:55:29 +0300

    Move logic to calculate neighbour articles to separate function
    - change some specs for doctree
    - add test to get-neighbour-entries fuction

--- test/cljdoc/doc_tree_test.clj
+++ test/cljdoc/doc_tree_test.clj
@@ -1,17 +1,19 @@
 (ns cljdoc.doc-tree-test
   (:require [cljdoc.doc-tree :as doctree]
             [clojure.test :as t]
-            [clojure.spec.test.alpha :as st]))
+            [clojure.spec.test.alpha :as st]
+            [clojure.spec.alpha :as spec]
+            [clojure.spec.gen.alpha :as gen]))
 
 (t/use-fixtures :once (fn [f] (st/instrument) (f)))
 
 (t/deftest process-toc-test
   (t/is (=
          [{:title "Readme"
-           :attrs {:cljdoc.doc/source-file "README.md",
+           :attrs {:cljdoc.doc/source-file "README.md"
                    :cljdoc.doc/type :cljdoc/markdown
                    :cljdoc.doc/contributors ["A" "B" "C"]
-                   :cljdoc/markdown "README.md",
+                   :cljdoc/markdown "README.md"
                    :slug "readme"}
            :children [{:title "Nested"
                        :attrs {:cljdoc.doc/source-file "nested.adoc"
@@ -24,3 +26,71 @@
            :get-contributors (constantly ["A" "B" "C"])}
           [["Readme" {:file "README.md"}
             ["Nested" {:file "nested.adoc"}]]]))))
+
+(spec/def ::entry
+  (spec/keys :req-un [:cljdoc.doc-tree/attrs :cljdoc.doc-tree/title]
+             :opt-un [::children]))
+
+(spec/def ::children
+  (spec/coll-of ::entry))
+
+(t/deftest find-neighbour-articles
+  (let [example-tree (doctree/add-slug-path (gen/sample (spec/gen ::entry) 3))]
+    ;; first element in tree will have no previous entry
+    (t/is (->> example-tree
+               first
+               :attrs
+               :slug-path
+               (doctree/get-neighbour-entries example-tree)
+               first
+               nil?))
+    ;; any non last entry in tree have two neighbour
+    (t/is (->> example-tree
+               second
+               :attrs
+               :slug-path
+               (doctree/get-neighbour-entries example-tree)
+               (every? (complement nil?))))
+    ;; last element in three have no next neighbour
+    (t/is (->> example-tree
+               doctree/flatten*
+               reverse
+               first
+               :attrs
+               :slug-path
+               (doctree/get-neighbour-entries example-tree)
+               last
+               nil?))))
+
+(comment
+  (def example-tree (doctree/add-slug-path (gen/sample (spec/gen ::entry) 3)))
+
+  (def test-slug-path (->> example-tree
+                           doctree/flatten*
+                           reverse
+                           first
+                           :attrs
+                           :slug-path))
+  test-slug-path
+  (count (filter #(-> %
+                      :attrs
+                      :slug-path
+                      (= test-slug-path)) (doctree/flatten* example-tree)))
+
+
+  (->> example-tree
+       second
+       :attrs
+       :slug-path
+       (doctree/get-neighbour-entries example-tree)
+       (every? (complement nil?)))
+
+  (->> example-tree
+       doctree/flatten*
+       reverse
+       first
+       :attrs
+       :slug-path
+       (doctree/get-neighbour-entries example-tree)
+       last
+       nil?))
