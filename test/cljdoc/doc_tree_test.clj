bcade298cbcedf854bbeb52c4b9707c343aa693e
Dmytro Palamarchuk <dmytro.p@attendify.com>
2020-08-17 17:47:57 +0300

    replace comments in test by testing macro

--- test/cljdoc/doc_tree_test.clj
+++ test/cljdoc/doc_tree_test.clj
@@ -27,6 +27,7 @@
           [["Readme" {:file "README.md"}
             ["Nested" {:file "nested.adoc"}]]]))))
 
+;; we redefine spec for entry because for test we want every entry have attrs with slug
 (spec/def ::entry
   (spec/keys :req-un [:cljdoc.doc-tree/attrs :cljdoc.doc-tree/title]
              :opt-un [::children]))
@@ -35,32 +36,33 @@
   (spec/coll-of ::entry))
 
 (t/deftest find-neighbour-articles
-  (let [example-tree (doctree/add-slug-path (gen/sample (spec/gen ::entry) 3))]
-    ;; first element in tree will have no previous entry
-    (t/is (->> example-tree
-               first
-               :attrs
-               :slug-path
-               (doctree/get-neighbour-entries example-tree)
-               first
-               nil?))
-    ;; any non last entry in tree have two neighbour
-    (t/is (->> example-tree
-               second
-               :attrs
-               :slug-path
-               (doctree/get-neighbour-entries example-tree)
-               (every? (complement nil?))))
-    ;; last element in three have no next neighbour
-    (t/is (->> example-tree
-               doctree/flatten*
-               reverse
-               first
-               :attrs
-               :slug-path
-               (doctree/get-neighbour-entries example-tree)
-               last
-               nil?))))
+  (t/testing "find neighbour entries in doc-tree by slug-path"
+    (let [example-tree (doctree/add-slug-path (gen/sample (spec/gen ::entry) 3))]
+      (t/testing "First element in tree will have no previous entry"
+        (t/is (->> example-tree
+                   first
+                   :attrs
+                   :slug-path
+                   (doctree/get-neighbour-entries example-tree)
+                   first
+                   nil?)))
+      (t/testing "Any not first and not last entry in tree have two neighbours"
+        (t/is (->> example-tree
+                   second
+                   :attrs
+                   :slug-path
+                   (doctree/get-neighbour-entries example-tree)
+                   (every? (complement nil?)))))
+      (t/testing "Last element in tree have no next neighbour"
+        (t/is (->> example-tree
+                   doctree/flatten*
+                   reverse
+                   first
+                   :attrs
+                   :slug-path
+                   (doctree/get-neighbour-entries example-tree)
+                   last
+                   nil?))))))
 
 (comment
   (def example-tree (doctree/add-slug-path (gen/sample (spec/gen ::entry) 3)))
