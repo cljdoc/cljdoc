76be7f68f32c593ca6aa67db803ca68b47c56bbd
rvalentini <ri.valentini@reply.de>
2021-09-14 17:22:37 +0200

    Add keyboard shortcuts for prev/next page navigation to help (#419)

--- src/cljdoc/render/meta.clj
+++ src/cljdoc/render/meta.clj
@@ -1,12 +1,20 @@
 (ns cljdoc.render.meta
   (:require [cljdoc.render.layout :as layout]))
 
+(defn- shortcut [key desc]
+  [:tr
+   [:td.pv2.pr4.pr7-ns.bb.b--black-20.nowrap key]
+   [:td.pv2.pr1.bb.b--black-20 desc]])
+
 (defn shortcuts []
   (->> [:div
         (layout/top-bar-generic)
         [:div.pa4-ns.pa2
          [:h1 "Shortcuts"]
-         [:div
-          [:p.fl.w-10.pa2.dib "⌘ K"]
-          [:p.fl.w-third.pa2.dib "Jump to recently viewed docs"]]]]
+         [:div.pa2.overflow-auto
+          [:table.f6.w-100.mw6 {:cellspacing 0}
+           [:tbody.lh-copy
+            (shortcut "⌘ K" "Jump to recently viewed docs")
+            (shortcut "←" "Move to previous page")
+            (shortcut "→" "Move to next page")]]]]]
        (layout/page {:title "shortcuts"})))
