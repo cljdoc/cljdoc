d22758403f3ae8705a95c8339b3a515ed9d9c604
Cora Sutton <cora@sutton.me>
2021-09-28 10:33:17 -0500

    Improve shortcut table styles

--- src/cljdoc/render/meta.clj
+++ src/cljdoc/render/meta.clj
@@ -2,19 +2,20 @@
   (:require [cljdoc.render.layout :as layout]))
 
 (defn- shortcut [key desc]
-  [:tr
-   [:td.pv2.pr4.pr7-ns.bb.b--black-20.nowrap key]
-   [:td.pv2.pr1.bb.b--black-20 desc]])
+  [:tr.striped--near-white
+   [:td.pv2.pr3.pl3.bb.b--black-20.nowrap key]
+   [:td.pv2.pr3.pl3.bb.b--black-20 desc]])
 
 (defn shortcuts []
   (->> [:div
         (layout/top-bar-generic)
-        [:div.pa4-ns.pa2
-         [:h1 "Shortcuts"]
-         [:div.pa2.overflow-auto
-          [:table.f6.w-100.mw6 {:cellspacing 0}
-           [:tbody.lh-copy
-            (shortcut "⌘ K" "Jump to recently viewed docs")
-            (shortcut "←" "Move to previous page")
-            (shortcut "→" "Move to next page")]]]]]
+        [:div.pa4-ns.pa2.w-auto.flex.justify-center
+         [:div
+          [:h1 "Shortcuts"]
+          [:div.overflow-auto
+           [:table.f6.w-auto.mw6.ba.br2.b--black-10 {:cellspacing 0}
+            [:tbody.lh-copy
+             (shortcut "⌘ K" "Jump to recently viewed docs")
+             (shortcut "←" "Move to previous page")
+             (shortcut "→" "Move to next page")]]]]]]
        (layout/page {:title "shortcuts"})))
