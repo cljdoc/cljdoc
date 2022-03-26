e57f0869f3579c9a2c1c1fca22ec85f73f6e477f
rvalentini <ri.valentini@reply.de>
2022-03-26 13:32:56 +0100

    Modify (and rename) complex hugsql queries so they can be used for both SQLite and Postgres queries

--- resources/sql/complex_queries.sql
+++ resources/sql/complex_queries.sql
@@ -1,11 +1,11 @@
 -- :name sql-resolve-version-ids :? :*
 -- For some reason putting WITH upfront instead of supplying (VALUES...) directly after IN is much more performant
 WITH v_ents(gid,aid,name) AS (VALUES :tuple*:version-entities)
-SELECT id, group_id, artifact_id, name FROM versions WHERE (group_id, artifact_id, name) IN v_ents
+SELECT id, group_id, artifact_id, name FROM versions WHERE (group_id, artifact_id, name) IN (SELECT * from v_ents)
 
 -- :name sql-get-namespaces :? :*
 select version_id, name, meta from namespaces where (version_id) IN :tuple:version-ids
 
 -- :name sql-get-vars :? :*
 WITH pairs(id, ns) AS (VALUES :tuple*:ns-idents)
-SELECT name, meta FROM vars WHERE (version_id, namespace) IN pairs
+SELECT name, meta FROM vars WHERE (version_id, namespace) IN (SELECT * from pairs)
