63615e7630fc57c1180c8863c6f9dd49140c3554
rvalentini <ri.valentini@reply.de>
2022-04-04 20:53:58 +0200

    Add postgres_migrations directory to migration tests

--- test/cljdoc/migration_test.clj
+++ test/cljdoc/migration_test.clj
@@ -5,32 +5,37 @@
             [clojure.test :as t]
             [me.raynes.fs :as fs]))
 
-(defn- migration-files []
-  (->> (fs/list-dir (io/resource "migrations"))
+(def dirs ["sqlite_migrations" "postgres_migrations"])
+
+(defn- migration-files [dir]
+  (->> (fs/list-dir (io/resource dir))
        (mapv #(fs/base-name (.getPath %)))))
 
 (t/deftest migrations-are-clj-or-sql
-  (doseq [mfile (migration-files)]
+  (doseq [dir dirs
+          mfile (migration-files dir)]
     (t/is (or (str/ends-with? mfile ".clj")
               (str/ends-with? mfile ".sql"))
           mfile)))
 
 (t/deftest migration-nums-are-sequential-and-have-no-gaps
-  (let [migration-nums (->> (migration-files)
-                            (mapv #(let [re-sep (if (str/ends-with? % ".sql") #"-" #"_")]
-                                     (first (str/split % re-sep))))
-                            distinct
-                            sort)
-        expected-nums (->> (range 1 (inc (count migration-nums)))
-                           (mapv #(format "%03d" %)))]
-    (t/is (= expected-nums migration-nums))))
+  (doseq [dir dirs]
+    (let [migration-nums (->> (migration-files dir)
+                           (mapv #(let [re-sep (if (str/ends-with? % ".sql") #"-" #"_")]
+                                    (first (str/split % re-sep))))
+                           distinct
+                           sort)
+          expected-nums (->> (range 1 (inc (count migration-nums)))
+                          (mapv #(format "%03d" %)))]
+      (t/is (= expected-nums migration-nums)))))
 
 (t/deftest sql-migrations-have-up-and-down
-  (let [sql-migrations (->> (migration-files)
-                            (filter #(str/ends-with? % ".sql"))
-                            (mapv #(rest (re-find #"(.+)\.(.+?).sql" %)))
-                            (reduce (fn [acc [mbase dir]]
-                                      (update acc mbase (fnil conj #{}) dir))
-                                    {}))]
-    (doseq [[mbase dirs] sql-migrations]
-      (t/is (= #{"up" "down"} dirs) mbase))))
+  (doseq [dir dirs]
+    (let [sql-migrations (->> (migration-files dir)
+                           (filter #(str/ends-with? % ".sql"))
+                           (mapv #(rest (re-find #"(.+)\.(.+?)\.sql" %)))
+                           (reduce (fn [acc [mbase dir]]
+                                     (update acc mbase (fnil conj #{}) dir))
+                             {}))]
+      (doseq [[mbase dirs] sql-migrations]
+        (t/is (= #{"up" "down"} dirs) mbase)))))
