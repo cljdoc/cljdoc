1f9cb08af1b092cbecec17b1240330c0dfbf5e2b
rvalentini <ri.valentini@reply.de>
2022-04-15 15:27:16 +0200

    Remove version from unused clojar_stats table, same as for sqlite migration

--- resources/postgres_migrations/005-create-stats-table.up.sql
+++ resources/postgres_migrations/005-create-stats-table.up.sql
@@ -3,7 +3,6 @@ CREATE TABLE clojars_stats
     date        text,
     group_id    text,
     artifact_id text,
-    version     text,
     downloads   integer,
-    constraint stats_keys unique (date, group_id, artifact_id, version)
+    constraint stats_keys unique (date, group_id, artifact_id)
 )
