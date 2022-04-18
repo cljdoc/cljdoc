39f1dfa15b8b598bc717dcc7b65e3a284d39b7c8
Cora Sutton <cora@sutton.me>
2022-04-18 18:01:41 -0500

    Format fix

--- test/cljdoc/render/api_searchset_test.clj
+++ test/cljdoc/render/api_searchset_test.clj
@@ -15,7 +15,6 @@
 ;; (let [searchset (api-searchset/cache-bundle->searchset cache-bundle)]
 ;;   (spit "resources/test_data/searchset.edn" (with-out-str (pp/pprint searchset))))
 
-
 (def doc (get-in cache-bundle [:version :doc 0]))
 
 (def version-entity (:version-entity cache-bundle))
