b57435873d6125d200092624bbd17d7f93297121
Cora Sutton <cora@sutton.me>
2022-04-18 18:02:36 -0500

    Use a comment block so it is easier to test

--- test/cljdoc/render/api_searchset_test.clj
+++ test/cljdoc/render/api_searchset_test.clj
@@ -11,9 +11,10 @@
                       slurp
                       edn/read-string))
 
-;; (require '[clojure.pprint :as pp])
-;; (let [searchset (api-searchset/cache-bundle->searchset cache-bundle)]
-;;   (spit "resources/test_data/searchset.edn" (with-out-str (pp/pprint searchset))))
+(comment
+  (require '[clojure.pprint])
+  (let [searchset (api-searchset/cache-bundle->searchset cache-bundle)]
+    (spit "resources/test_data/searchset.edn" (with-out-str (clojure.pprint/pprint searchset)))))
 
 (def doc (get-in cache-bundle [:version :doc 0]))
 
