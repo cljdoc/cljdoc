5f91be75d1ecbb84cc1c25a8238bfe287801cb7e
lread <lee@dlread.com>
2023-08-09 19:01:30 -0400

    ops: upgrade from terraform 0.12 to 0.13
    changes made by `terraform 0.13upgrade` via terraform 0.13.7

--- ops/infrastructure/versions.tf
+++ ops/infrastructure/versions.tf
@@ -1,4 +1,12 @@
 
 terraform {
-  required_version = ">= 0.12"
+  required_version = ">= 0.13"
+  required_providers {
+    aws = {
+      source = "hashicorp/aws"
+    }
+    digitalocean = {
+      source = "digitalocean/digitalocean"
+    }
+  }
 }
