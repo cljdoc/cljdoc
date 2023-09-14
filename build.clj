92e9640ff5223f90abc7641377954fdf9dd8d064
lread <lee@dlread.com>
2023-09-14 17:54:47 -0400

    lint: tweak clojure vs bb deps
    Use new "bb print-deps" for full list of bb dependencies including
    built-ins. Tack this onto clojure deps when calculating classpath for
    lint cache build.
    
    To support Windows, with its limited command-line length, move to
    building cache programmatically in clojure in build.clj.
    
    This is not perfect, if a bb dep version is older than a clojure dep version,
    we'll end up using the older dep, but it is better than before and unlikely to
    cause issues.

--- build.clj
+++ build.clj
@@ -1,7 +1,12 @@
 (ns build
   (:require
    [build-shared :as bs]
-   [clojure.tools.build.api :as b]))
+   [babashka.fs :as fs]
+   [babashka.process :as process]
+   [clojure.string :as str]
+   [clojure.edn :as edn]
+   [clojure.tools.build.api :as b]
+   [clj-kondo.core :as kondo]))
 
 (def basis (b/create-basis {:project "deps.edn"}))
 
@@ -13,3 +18,19 @@
             :javac-opts ["--release" "17"
                          "-Xlint:all,-serial,-processing"
                          "-Werror"]} ))
+
+(defn lint-cache
+  "Build lint cache as a build task to avoid blowing max command line length on Windows"
+  [_opts]
+  (fs/delete-tree ".clj-kondo/.cache")
+  (let [bb-deps (-> (process/shell {:out :string} "bb print-deps")
+                    :out
+                    edn/read-string)
+        ;; ideally we'd be smarter about merging bb-deps, i.e, choosing latest version
+        ;; for any libs that overlap, but this should be good enough for now
+        basis (b/create-basis {:aliases [:test :cli] :extra bb-deps})
+        cp-roots (:classpath-roots basis)]
+    (println "- copying configs")
+    (kondo/run! {:skip-lint true :copy-configs true :lint cp-roots})
+    (println "\n- creating cache")
+    (kondo/run! {:dependencies true :parallel true :lint cp-roots})))
