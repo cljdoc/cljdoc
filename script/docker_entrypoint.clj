e30f945589d3c93c65b85bd60a07af97f91c2890
lread <lee@dlread.com>
2023-09-14 14:39:10 -0400

    code format check/fix

--- script/docker_entrypoint.clj
+++ script/docker_entrypoint.clj
@@ -31,27 +31,27 @@
       (run! (fn [f]
               (println (str "- " f))
               (fs/delete f))
-            old-dumps))) )
+            old-dumps))))
 
 (defn -main [& _args]
   (println "Preparing heap dump dir")
   (let [data-dir (-> (t/clojure {:out :string}
-                              "-M script/get_cljdoc_data_dir.clj")
-                   :out
-                   str/trim)
-      heap-dump-dir (fs/file data-dir "heapdumps")
-      heap-dump-file (fs/file heap-dump-dir "heapdump.hprof")]
-  (println "- will save heap dump on out of memory error to:" (str heap-dump-file))
-  (fs/create-dirs heap-dump-dir)
-  (preserve-heap-dump heap-dump-file heap-dump-dir)
-  (turf-old-heap-dumps heap-dump-dir)
-  (println "Launching cljdoc server")
-  #_:clj-kondo/ignore ;; bb.edn requires etaoin wich requires an older version of process that has a more limited signature
-  (process/exec "clojure"
-                "-J-Dcljdoc.host=0.0.0.0"
-                "-J-XX:+HeapDumpOnOutOfMemoryError"
-                (format "-J-XX:HeapDumpPath=%s" (str heap-dump-file))
-                "-M" "-m" "cljdoc.server.system")))
+                                "-M script/get_cljdoc_data_dir.clj")
+                     :out
+                     str/trim)
+        heap-dump-dir (fs/file data-dir "heapdumps")
+        heap-dump-file (fs/file heap-dump-dir "heapdump.hprof")]
+    (println "- will save heap dump on out of memory error to:" (str heap-dump-file))
+    (fs/create-dirs heap-dump-dir)
+    (preserve-heap-dump heap-dump-file heap-dump-dir)
+    (turf-old-heap-dumps heap-dump-dir)
+    (println "Launching cljdoc server")
+    #_:clj-kondo/ignore ;; bb.edn requires etaoin wich requires an older version of process that has a more limited signature
+    (process/exec "clojure"
+                  "-J-Dcljdoc.host=0.0.0.0"
+                  "-J-XX:+HeapDumpOnOutOfMemoryError"
+                  (format "-J-XX:HeapDumpPath=%s" (str heap-dump-file))
+                  "-M" "-m" "cljdoc.server.system")))
 
 (when (= *file* (System/getProperty "babashka.file"))
   (apply -main *command-line-args*))
