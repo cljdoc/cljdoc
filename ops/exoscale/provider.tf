2ebed3f1b6d709a33d4578ca73a3322a59cf7ead
lread <lee@dlread.com>
2024-09-06 10:05:32 -0400

    terraform: mark exoscale vars as sensitive
    Also: gitignore secrets.tfvars

--- ops/exoscale/provider.tf
+++ ops/exoscale/provider.tf
@@ -13,8 +13,16 @@ terraform {
   required_version = ">= 1.9.5"
 }
 
-variable "exoscale_api_key" { type = string }
-variable "exoscale_api_secret" { type = string }
+variable "exoscale_api_key" {
+  type = string
+  sensitive = true
+}
+
+variable "exoscale_api_secret" {
+  type = string
+  sensitive = true
+}
+
 variable "exoscale_zone" {
   type    = string
   default = "ch-gva-2"
