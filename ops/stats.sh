dc9c8366d0aea6883252366e5408b1ec4acc9d52
lread <lee@dlread.com>
2024-09-24 17:14:13 -0400

    Move digitalocean dirs under ops/digitalocean
    This will be soon be only in git history, but for now it is handy to
    refer to - and if things really go sideways fall-back to, if necessary.
    
    Moved ./modules/deploy under ./ops/digitalocean.
    
    So we now have matching infrastructure/, image/, and deploy/ under
    ./ops/exoscale and ./ops/digitalocean

--- ops/stats.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-set -eou pipefail
-
-ssh root@cljdoc.org cat /var/log/nginx/cljdoc-access.log \
-    | goaccess -c \
-               --log-format='%h %^[%d:%t %^] "%r" %s %b "%R" "%u" %^ %T' \
-               --date-format '%d/%b/%Y' \
-               --time-format '%H:%M:%S' -
