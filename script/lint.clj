1e7f5926f928ae3f7cc959d76c8e86634b64ed03
lread <lee@dlread.com>
2024-09-26 11:01:23 -0400

    linting: fix (surprisingly harmless) typo

--- script/lint.clj
+++ script/lint.clj
@@ -32,7 +32,7 @@
   (status/line :head "clj-kondo: linting")
   (let [{:keys [exit]}
         (shell/command {:continue true}
-                       "clojure -M:clj-kondo -m clj-kond.main --lint src test script deps.edn bb.edn modules ops/exoscale/deploy")]
+                       "clojure -M:clj-kondo --lint src test script deps.edn bb.edn modules ops/exoscale/deploy")]
     (cond
       (= 2 exit) (status/die exit "clj-kondo found one or more lint errors")
       (= 3 exit) (status/die exit "clj-kondo found one or more lint warnings")
