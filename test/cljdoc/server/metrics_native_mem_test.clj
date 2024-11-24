8e1ffcc5d71370b961fc7591d02d1569dcc4cd78
lread <lee@dlread.com>
2024-11-24 17:20:55 -0500

    test: deliberate values for native mem data parse

--- test/cljdoc/server/metrics_native_mem_test.clj
+++ test/cljdoc/server/metrics_native_mem_test.clj
@@ -17,220 +17,221 @@
   (t/is (match?
          (m/nested-equals
           [{:total
-            {:reserved {:kb 2724942},
-             :committed {:kb 1497650},
-             :malloc {:kb 130882, :cnt 951709, :peak {:kb 142074, :cnt 947473}},
-             :mmap {:reserved {:kb 2594060}, :committed {:kb 1366768}}}}
+            {:reserved {:kb 1001},
+             :committed {:kb 1002},
+             :malloc {:kb 1003, :cnt 1004, :peak {:kb 1005, :cnt 1006}},
+             :mmap {:reserved {:kb 1007}, :committed {:kb 1008}}}}
            {:java-heap
-            {:reserved {:kb 1048576},
-             :committed {:kb 1048576},
+            {:reserved {:kb 2001},
+             :committed {:kb 2002},
              :mmap
-             {:reserved {:kb 1048576}, :committed {:kb 1048576, :at-peak true}}}}
+             {:reserved {:kb 2003}, :committed {:kb 2004, :at-peak true}}}}
            {:class
-            {:reserved {:kb 1072866},
-             :committed {:kb 71906},
+            {:reserved {:kb 3001},
+             :committed {:kb 3002},
              :classes
-             {:cnt 39698,
-              :instance-classes {:cnt 38579},
-              :array-classes {:cnt 1119}},
-             :malloc {:kb 24290, :cnt 377958, :at-peak true},
+             {:cnt 3003,
+              :instance-classes {:cnt 3004},
+              :array-classes {:cnt 3005}},
+             :malloc {:kb 3006, :cnt 3007, :at-peak true},
              :mmap
-             {:reserved {:kb 1048576},
-              :committed {:kb 47616, :at-peak true},
+             {:reserved {:kb 3008},
+              :committed {:kb 3009, :at-peak true},
               :metadata
-              {:reserved {:kb 196608},
-               :committed {:kb 169664},
-               :used {:kb 146997},
-               :waste {:kb 22667}},
+              {:reserved {:kb 3010},
+               :committed {:kb 3011},
+               :used {:kb 3012},
+               :waste {:kb 3013}},
               :class-space
-              {:reserved {:kb 1048576},
-               :committed {:kb 47616},
-               :used {:kb 36517},
-               :waste {:kb 11099}}}}}
+              {:reserved {:kb 3014},
+               :committed {:kb 3015},
+               :used {:kb 3016},
+               :waste {:kb 3017}}}}}
            {:thread
-            {:reserved {:kb 32872},
-             :committed {:kb 3848},
-             :threads {:cnt 32},
+            {:reserved {:kb 4001},
+             :committed {:kb 4002},
+             :threads {:cnt 4003},
              :stack
-             {:reserved {:kb 32768}, :committed {:kb 3744, :peak {:kb 3744}}},
-             :malloc {:kb 69, :cnt 198, :peak {:kb 95, :cnt 251}},
-             :arena {:kb 36, :cnt 62, :peak {:kb 1313, :cnt 60}}}}
+             {:reserved {:kb 4004}, :committed {:kb 4005, :peak {:kb 4006}}},
+             :malloc {:kb 4007, :cnt 4008, :peak {:kb 4009 :cnt 4010}},
+             :arena {:kb 4011, :cnt 4012, :peak {:kb 4013, :cnt 4014}}}}
            {:code
-            {:reserved {:kb 287847},
-             :committed {:kb 119951},
-             :malloc {:kb 40159, :cnt 96615, :at-peak true},
+            {:reserved {:kb 5001},
+             :committed {:kb 5002},
+             :malloc {:kb 5003, :cnt 5004, :at-peak true},
              :mmap
-             {:reserved {:kb 247688}, :committed {:kb 79792, :at-peak true}},
-             :arena {:kb 0, :cnt 0, :peak {:kb 34, :cnt 2}}}}
+             {:reserved {:kb 5005}, :committed {:kb 5006, :at-peak true}},
+             :arena {:kb 5007, :cnt 5008, :peak {:kb 5009, :cnt 5010}}}}
            {:gc
-            {:reserved {:kb 3438},
-             :committed {:kb 3438},
-             :malloc {:kb 22, :cnt 58, :peak {:kb 752, :cnt 243}},
-             :mmap {:reserved {:kb 3416}, :committed {:kb 3416, :at-peak true}}}}
+            {:reserved {:kb 6001},
+             :committed {:kb 6002},
+             :malloc {:kb 6003, :cnt 6004, :peak {:kb 6005, :cnt 6006}},
+             :mmap {:reserved {:kb 6007}, :committed {:kb 6008, :at-peak true}}}}
            {:compiler
-            {:reserved {:kb 402},
-             :committed {:kb 402},
-             :malloc {:kb 238, :cnt 250, :peak {:kb 308, :cnt 276}},
-             :arena {:kb 164, :cnt 4, :peak {:kb 38881, :cnt 13}}}}
+            {:reserved {:kb 7001},
+             :committed {:kb 7002},
+             :malloc {:kb 7003, :cnt 7004, :peak {:kb 7005, :cnt 7006}},
+             :arena {:kb 7007, :cnt 7008, :peak {:kb 7009, :cnt 7010}}}}
            {:internal
-            {:reserved {:kb 1478},
-             :committed {:kb 1478},
-             :malloc {:kb 1442, :cnt 52086, :at-peak true},
-             :mmap {:reserved {:kb 36}, :committed {:kb 36, :at-peak true}}}}
+            {:reserved {:kb 8001},
+             :committed {:kb 8002},
+             :malloc {:kb 8003, :cnt 8004, :at-peak true},
+             :mmap {:reserved {:kb 8005}, :committed {:kb 8006, :at-peak true}}}}
            {:other
-            {:reserved {:kb 225},
-             :committed {:kb 225},
-             :malloc {:kb 225, :cnt 42, :peak {:kb 230, :cnt 43}}}}
+            {:reserved {:kb 9001},
+             :committed {:kb 9002},
+             :malloc {:kb 9003, :cnt 9004, :peak {:kb 9005, :cnt 9006}}}}
            {:symbol
-            {:reserved {:kb 30464},
-             :committed {:kb 30464},
-             :malloc {:kb 26412, :cnt 346926, :at-peak true},
-             :arena {:kb 4052, :cnt 1, :at-peak true}}}
+            {:reserved {:kb 10001},
+             :committed {:kb 10002},
+             :malloc {:kb 10003, :cnt 10004, :at-peak true},
+             :arena {:kb 10005, :cnt 10006, :at-peak true}}}
            {:native-memory-tracking
-            {:reserved {:kb 15115},
-             :committed {:kb 15115},
-             :malloc {:kb 245, :cnt 4449, :at-peak true},
-             :tracking-overhead {:kb 14870}}}
+            {:reserved {:kb 11001},
+             :committed {:kb 11002},
+             :malloc {:kb 11003, :cnt 11004, :at-peak true},
+             :tracking-overhead {:kb 11005}}}
            {:shared-class-space
-            {:reserved {:kb 16384},
-             :committed {:kb 13916},
-             :readonly {:kb 0},
+            {:reserved {:kb 12001},
+             :committed {:kb 12002},
+             :readonly {:kb 12003},
              :mmap
-             {:reserved {:kb 16384}, :committed {:kb 13916, :peak {:kb 14100}}}}}
+             {:reserved {:kb 12004}, :committed {:kb 12005, :peak {:kb 12006}}}}}
            {:arena-chunk
-            {:reserved {:kb 1745},
-             :committed {:kb 1745},
-             :malloc {:kb 1745, :cnt 247, :peak {:kb 43026, :cnt 1114}}}}
+            {:reserved {:kb 13001},
+             :committed {:kb 13002},
+             :malloc {:kb 13003, :cnt 13004, :peak {:kb 13005, :cnt 13006}}}}
            {:tracing
-            {:reserved {:kb 32},
-             :committed {:kb 32},
-             :arena {:kb 32, :cnt 1, :at-peak true}}}
+            {:reserved {:kb 14001},
+             :committed {:kb 14002},
+             :arena {:kb 14003, :cnt 14004, :at-peak true}}}
            {:module
-            {:reserved {:kb 10688},
-             :committed {:kb 10688},
-             :malloc {:kb 10688, :cnt 45253, :at-peak true}}}
+            {:reserved {:kb 15001},
+             :committed {:kb 15002},
+             :malloc {:kb 15003, :cnt 15004, :at-peak true}}}
            {:safepoint
-            {:reserved {:kb 8},
-             :committed {:kb 8},
-             :mmap {:reserved {:kb 8}, :committed {:kb 8, :at-peak true}}}}
+            {:reserved {:kb 16001},
+             :committed {:kb 16002},
+             :mmap {:reserved {:kb 16003}, :committed {:kb 16004, :at-peak true}}}}
            {:synchronization
-            {:reserved {:kb 2255},
-             :committed {:kb 2255},
-             :malloc {:kb 2255, :cnt 22186, :at-peak true}}}
+            {:reserved {:kb 17001},
+             :committed {:kb 17002},
+             :malloc {:kb 17003, :cnt 17004, :at-peak true}}}
            {:serviceability
-            {:reserved {:kb 18},
-             :committed {:kb 18},
-             :malloc {:kb 18, :cnt 28, :at-peak true}}}
+            {:reserved {:kb 18001},
+             :committed {:kb 18002},
+             :malloc {:kb 18003, :cnt 18004, :at-peak true}}}
            {:metaspace
-            {:reserved {:kb 200528},
-             :committed {:kb 173584},
-             :malloc {:kb 3920, :cnt 5390, :at-peak true},
+            {:reserved {:kb 19001},
+             :committed {:kb 19002},
+             :malloc {:kb 19003, :cnt 19004, :at-peak true},
              :mmap
-             {:reserved {:kb 196608}, :committed {:kb 169664, :at-peak true}}}}
+             {:reserved {:kb 19005}, :committed {:kb 19006, :at-peak true}}}}
            {:string-deduplication
-            {:reserved {:kb 1},
-             :committed {:kb 1},
-             :malloc {:kb 1, :cnt 8, :at-peak true}}}
+            {:reserved {:kb 20001},
+             :committed {:kb 20002},
+             :malloc {:kb 20003, :cnt 20004, :at-peak true}}}
            {:object-monitors
-            {:reserved {:kb 0},
-             :committed {:kb 0},
-             :malloc {:kb 0, :cnt 2, :peak {:kb 1223, :cnt 6262}}}}
+            {:reserved {:kb 21001},
+             :committed {:kb 21002},
+             :malloc {:kb 21003, :cnt 21004 :peak {:kb 21005, :cnt 21006}}}}
            {:unknown
-            {:reserved {:kb 0},
-             :committed {:kb 0},
-             :mmap {:reserved {:kb 0}, :committed {:kb 0, :peak {:kb 20}}}}}])
+            {:reserved {:kb 22001},
+             :committed {:kb 22002},
+             :mmap {:reserved {:kb 22003}, :committed {:kb 22004, :peak {:kb 22005}}}}}])
+         ;; grabbed from real output, with numeric vals changed for testing
          (mnm/parse-output-text "
 
 Native Memory Tracking:
 
 (Omitting categories weighting less than 1KB)
 
-Total: reserved=2724942KB, committed=1497650KB
-       malloc: 130882KB #951709, peak=142074KB #947473
-       mmap:   reserved=2594060KB, committed=1366768KB
+Total: reserved=1001KB, committed=1002KB
+       malloc: 1003KB #1004, peak=1005KB #1006
+       mmap:   reserved=1007KB, committed=1008KB
 
--                 Java Heap (reserved=1048576KB, committed=1048576KB)
-                            (mmap: reserved=1048576KB, committed=1048576KB, at peak)
+-                 Java Heap (reserved=2001KB, committed=2002KB)
+                            (mmap: reserved=2003KB, committed=2004KB, at peak)
 
--                     Class (reserved=1072866KB, committed=71906KB)
-                            (classes #39698)
-                            (  instance classes #38579, array classes #1119)
-                            (malloc=24290KB #377958) (at peak)
-                            (mmap: reserved=1048576KB, committed=47616KB, at peak)
+-                     Class (reserved=3001KB, committed=3002KB)
+                            (classes #3003)
+                            (  instance classes #3004, array classes #3005)
+                            (malloc=3006KB #3007) (at peak)
+                            (mmap: reserved=3008KB, committed=3009KB, at peak)
                             (  Metadata:   )
-                            (    reserved=196608KB, committed=169664KB)
-                            (    used=146997KB)
-                            (    waste=22667KB =13.36%)
+                            (    reserved=3010KB, committed=3011KB)
+                            (    used=3012KB)
+                            (    waste=3013KB =13.36%)
                             (  Class space:)
-                            (    reserved=1048576KB, committed=47616KB)
-                            (    used=36517KB)
-                            (    waste=11099KB =23.31%)
+                            (    reserved=3014KB, committed=3015KB)
+                            (    used=3016KB)
+                            (    waste=3017KB =23.31%)
 
--                    Thread (reserved=32872KB, committed=3848KB)
-                            (threads #32)
-                            (stack: reserved=32768KB, committed=3744KB, peak=3744KB)
-                            (malloc=69KB #198) (peak=95KB #251)
-                            (arena=36KB #62) (peak=1313KB #60)
+-                    Thread (reserved=4001KB, committed=4002KB)
+                            (threads #4003)
+                            (stack: reserved=4004KB, committed=4005KB, peak=4006KB)
+                            (malloc=4007KB #4008) (peak=4009KB #4010)
+                            (arena=4011KB #4012) (peak=4013KB #4014)
 
--                      Code (reserved=287847KB, committed=119951KB)
-                            (malloc=40159KB #96615) (at peak)
-                            (mmap: reserved=247688KB, committed=79792KB, at peak)
-                            (arena=0KB #0) (peak=34KB #2)
+-                      Code (reserved=5001KB, committed=5002KB)
+                            (malloc=5003KB #5004) (at peak)
+                            (mmap: reserved=5005KB, committed=5006KB, at peak)
+                            (arena=5007KB #5008) (peak=5009KB #5010)
 
--                        GC (reserved=3438KB, committed=3438KB)
-                            (malloc=22KB #58) (peak=752KB #243)
-                            (mmap: reserved=3416KB, committed=3416KB, at peak)
+-                        GC (reserved=6001KB, committed=6002KB)
+                            (malloc=6003KB #6004) (peak=6005KB #6006)
+                            (mmap: reserved=6007KB, committed=6008KB, at peak)
 
--                  Compiler (reserved=402KB, committed=402KB)
-                            (malloc=238KB #250) (peak=308KB #276)
-                            (arena=164KB #4) (peak=38881KB #13)
+-                  Compiler (reserved=7001KB, committed=7002KB)
+                            (malloc=7003KB #7004) (peak=7005KB #7006)
+                            (arena=7007KB #7008) (peak=7009KB #7010)
 
--                  Internal (reserved=1478KB, committed=1478KB)
-                            (malloc=1442KB #52086) (at peak)
-                            (mmap: reserved=36KB, committed=36KB, at peak)
+-                  Internal (reserved=8001KB, committed=8002KB)
+                            (malloc=8003KB #8004) (at peak)
+                            (mmap: reserved=8005KB, committed=8006KB, at peak)
 
--                     Other (reserved=225KB, committed=225KB)
-                            (malloc=225KB #42) (peak=230KB #43)
+-                     Other (reserved=9001KB, committed=9002KB)
+                            (malloc=9003KB #9004) (peak=9005KB #9006)
 
--                    Symbol (reserved=30464KB, committed=30464KB)
-                            (malloc=26412KB #346926) (at peak)
-                            (arena=4052KB #1) (at peak)
+-                    Symbol (reserved=10001KB, committed=10002KB)
+                            (malloc=10003KB #10004) (at peak)
+                            (arena=10005KB #10006) (at peak)
 
--    Native Memory Tracking (reserved=15115KB, committed=15115KB)
-                            (malloc=245KB #4449) (at peak)
-                            (tracking overhead=14870KB)
+-    Native Memory Tracking (reserved=11001KB, committed=11002KB)
+                            (malloc=11003KB #11004) (at peak)
+                            (tracking overhead=11005KB)
 
--        Shared class space (reserved=16384KB, committed=13916KB, readonly=0KB)
-                            (mmap: reserved=16384KB, committed=13916KB, peak=14100KB)
+-        Shared class space (reserved=12001KB, committed=12002KB, readonly=12003KB)
+                            (mmap: reserved=12004KB, committed=12005KB, peak=12006KB)
 
--               Arena Chunk (reserved=1745KB, committed=1745KB)
-                            (malloc=1745KB #247) (peak=43026KB #1114)
+-               Arena Chunk (reserved=13001KB, committed=13002KB)
+                            (malloc=13003KB #13004) (peak=13005KB #13006)
 
--                   Tracing (reserved=32KB, committed=32KB)
-                            (arena=32KB #1) (at peak)
+-                   Tracing (reserved=14001KB, committed=14002KB)
+                            (arena=14003KB #14004) (at peak)
 
--                    Module (reserved=10688KB, committed=10688KB)
-                            (malloc=10688KB #45253) (at peak)
+-                    Module (reserved=15001KB, committed=15002KB)
+                            (malloc=15003KB #15004) (at peak)
 
--                 Safepoint (reserved=8KB, committed=8KB)
-                            (mmap: reserved=8KB, committed=8KB, at peak)
+-                 Safepoint (reserved=16001KB, committed=16002KB)
+                            (mmap: reserved=16003KB, committed=16004KB, at peak)
 
--           Synchronization (reserved=2255KB, committed=2255KB)
-                            (malloc=2255KB #22186) (at peak)
+-           Synchronization (reserved=17001KB, committed=17002KB)
+                            (malloc=17003KB #17004) (at peak)
 
--            Serviceability (reserved=18KB, committed=18KB)
-                            (malloc=18KB #28) (at peak)
+-            Serviceability (reserved=18001KB, committed=18002KB)
+                            (malloc=18003KB #18004) (at peak)
 
--                 Metaspace (reserved=200528KB, committed=173584KB)
-                            (malloc=3920KB #5390) (at peak)
-                            (mmap: reserved=196608KB, committed=169664KB, at peak)
+-                 Metaspace (reserved=19001KB, committed=19002KB)
+                            (malloc=19003KB #19004) (at peak)
+                            (mmap: reserved=19005KB, committed=19006KB, at peak)
 
--      String Deduplication (reserved=1KB, committed=1KB)
-                            (malloc=1KB #8) (at peak)
+-      String Deduplication (reserved=20001KB, committed=20002KB)
+                            (malloc=20003KB #20004) (at peak)
 
--           Object Monitors (reserved=0KB, committed=0KB)
-                            (malloc=0KB #2) (peak=1223KB #6262)
+-           Object Monitors (reserved=21001KB, committed=21002KB)
+                            (malloc=21003KB #21004) (peak=21005KB #21006)
 
--                   Unknown (reserved=0KB, committed=0KB)
-                            (mmap: reserved=0KB, committed=0KB, peak=20KB)
+-                   Unknown (reserved=22001KB, committed=22002KB)
+                            (mmap: reserved=22003KB, committed=22004KB, peak=22005KB)
 "))))
