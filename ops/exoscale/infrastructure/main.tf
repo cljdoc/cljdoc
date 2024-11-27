eaa0a16c4b8614854e35f4919ce476202d518798
lread <lee@dlread.com>
2024-11-27 15:50:06 -0500

    ops: terraform: deleted moved after apply

--- ops/exoscale/infrastructure/main.tf
+++ ops/exoscale/infrastructure/main.tf
@@ -65,10 +65,3 @@ output "cljdoc_instance_ip" {
 output "cljdoc_static_ip" {
   value = exoscale_elastic_ip.cljdoc_prod.ip_address
 }
-
-# To support refactor, delete after applied
-
-moved {
-  from = module.main_server.exoscale_ssh_key.cljdoc_base_ssh_key
-  to = exoscale_ssh_key.cljdoc_base_ssh_key
-}
