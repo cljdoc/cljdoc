9689d66662a717a438dfcb36b3d099a5d2f65e7f
Cora Sutton <cora@sutton.me>
2025-01-04 00:31:33 -0600

    Add a simple script for building

---
+++ script/build_js.sh
@@ -0,0 +1,53 @@
+#!/bin/sh
+
+# Compile the JS
+npx \
+  --yes \
+  esbuild \
+  --bundle \
+  js/index.tsx \
+  --target=es2022 \
+  --bundle \
+  --sourcemap \
+  --minify \
+  --allow-overwrite \
+  --entry-names='[dir]/[name].[hash]' \
+  --outfile=resources-compiled/public/out/cljdoc.js
+
+# Copy the static files that need hashing
+npx \
+  --yes \
+  esbuild \
+  --allow-overwrite \
+  --entry-names='[dir]/[name].[hash]' \
+  --loader:.css=copy \
+  --loader:.svg=copy \
+  --loader:.png=copy \
+  --loader:.html=copy \
+  --outdir=resources-compiled/public/out \
+  resources/public/*.*
+
+# Copy the CSS files and minify them
+npx \
+  --yes \
+  esbuild \
+  --minify \
+  --allow-overwrite \
+  --entry-names='[dir]/[name].[hash]' \
+  --outdir=resources-compiled/public/out \
+  resources/public/*.css
+
+# Copy the static files that don't need hashing
+npx \
+  --yes \
+  esbuild \
+  --minify \
+  --allow-overwrite \
+  --entry-names='[dir]/[name]' \
+  --loader:.ico=copy \
+  --loader:.css=copy \
+  --loader:.svg=copy \
+  --loader:.png=copy \
+  --loader:.html=copy \
+  --outdir=resources-compiled/public/out \
+  resources/public/static/*.*
