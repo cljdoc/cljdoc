ffc265a39796c296105582a423c6576801607b04
lread <lee@dlread.com>
2025-07-15 13:54:28 -0400

    sentry: experiment with logging logger errors
    It would be nice to see when the sentry appender cannot log.

--- src/cljdoc/server/log_init.clj
+++ src/cljdoc/server/log_init.clj
@@ -1,15 +1,23 @@
 (ns cljdoc.server.log-init
   "Some libraries have unusual default logging, jetty for instance can emit DEBUG lines.
   We've separated out log-init to allow it to be easily required from other nses."
-  (:require [cljdoc.util.sentry :as sentry]
+  (:require [babashka.fs :as fs]
+            [cljdoc.util.sentry :as sentry]
             [unilog.config :as unilog])
-  (:import [ch.qos.logback.classic Level]
-           [ch.qos.logback.classic.spi ILoggingEvent ThrowableProxy]
-           [ch.qos.logback.core UnsynchronizedAppenderBase]))
+  (:import [ch.qos.logback.classic Level LoggerContext]
+           [ch.qos.logback.classic.spi ILoggingEvent ThrowableProxy Configurator]
+           [ch.qos.logback.core UnsynchronizedAppenderBase]
+           [ch.qos.logback.core.status OnFileStatusListener StatusManager StatusListener]
+           [ch.qos.logback.core.util StatusPrinter2]
+           [java.io PrintStream]
+           [org.slf4j LoggerFactory]))
 
 (set! *warn-on-reflection* true)
 
-(def log-file (or (System/getenv "CLJDOC_LOG_FILE") "log/cljdoc.log"))
+(def log-file (str (fs/absolutize (or (System/getenv "CLJDOC_LOG_FILE") "log/cljdoc.log"))))
+(def log-dir (str (fs/parent log-file)) )
+(def logger-file (str (fs/path log-dir "logger.log")))
+(fs/create-dirs log-dir)
 
 (defn log-event->map
   "Convert log event for consumption by sentry"
@@ -30,9 +38,8 @@
          (proxy [UnsynchronizedAppenderBase] []
            (start []
              (if-not (:sentry-dsn config)
-               ;; TODO consider a custom error logger for errors when trying to talk to sentry
-               ;; TODO if we dont' call `start` on super I expect this means appender is not started?
-               (println "SENTRY_WARN: Sentry DSN not configured, errors will not be sent to sentry.io.")
+               (let [^UnsynchronizedAppenderBase this this]
+                 (proxy-super addWarn "Sentry DSN not configured, logged errors will not be sent to sentry.io. This is normal for local dev."))
                (let [^UnsynchronizedAppenderBase this this]
                  (proxy-super start))))
            (append [^ILoggingEvent event]
@@ -45,9 +52,8 @@
                    (let [log-event (log-event->map event)]
                      (sentry/capture-log-event config log-event)))
                  (catch Throwable t
-                   ;; TODO consider a custom error logger for errors when trying to talk to sentry
-                   (println "SENTRY_ERROR: unable to send log event to sentry.io:" event t))))))))
-
+                   (let [^UnsynchronizedAppenderBase this this]
+                     (proxy-super addError (str "Unable to forward log event event to sentry.io:" event) t)))))))))
 
 (unilog/start-logging!
  {:level   :info
@@ -60,3 +66,13 @@
                :rolling-policy {:type :time-based
                                 :max-history 14 ;; days (based on pattern)
                                 :pattern "%d{yyyy-MM-dd}"}}]})
+
+;; register a status listener so that we can see "status" errors from our appender
+(let [^LoggerContext lc (LoggerFactory/getILoggerFactory)
+      ^StatusManager status-manager (.getStatusManager lc)
+      ^StatusListener listener (doto (OnFileStatusListener.)
+                         (.setFilename logger-file))]
+  (.add status-manager listener)
+  ;; Print any pre-existing status messages
+  (.print (doto (StatusPrinter2.)
+            (.setPrintStream (PrintStream. (fs/file logger-file)))) status-manager))
