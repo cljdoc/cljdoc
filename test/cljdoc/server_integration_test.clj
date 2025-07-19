a806688ebc865ecc4797e756e0a96b0e481786a3
lread <lee@dlread.com>
2025-07-19 11:26:29 -0400

    integration test: start up all except pedestal
    Probably a bit better to give everything we can a shake.

--- test/cljdoc/server_integration_test.clj
+++ test/cljdoc/server_integration_test.clj
@@ -51,7 +51,7 @@
   (st/instrument)
   (let [port (+ 8000 (rand-int 1000))
         opensearch-base-url (format "http://localhost:%d" port)
-        cfg {:cljdoc/server {:port port
+        env-cfg {:cljdoc/server {:port port
                              :opensearch-base-url opensearch-base-url
                              :analysis-service :local
                              :autobuild-clojars-releases? false
@@ -59,8 +59,12 @@
                              :enable-db-restore? false
                              :clojars-stats-retention-days 5
                              :dir test-data-dir}
-             :cljdoc/version "some-version"}]
-    (ig/init (sys/system-config cfg) [:cljdoc/pedestal-connector])))
+             :cljdoc/version "some-version"}
+        sys-cfg (sys/system-config env-cfg)]
+    ;; start everything up, including cljdoc/pedestal-connector but don't start pedestal
+    (ig/init sys-cfg (->> sys-cfg
+                          keys
+                          (remove #{:cljdoc/pedestal})))))
 
 (defn halt! [system]
   (ig/halt! system)
