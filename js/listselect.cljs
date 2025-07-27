f6ae54ab2a6782135b602b43aa66c4ca769cb583
lread <lee@dlread.com>
2025-07-26 21:51:54 -0400

    wip: squinting on listselect

--- js/listselect.cljs
+++ js/listselect.cljs
@@ -1,6 +1,5 @@
 (ns listselect
-  (:require ["preact" :refer [Component]]
-            [squint.core :refer [defclass]]))
+  (:require ["preact/hooks" :refer [useRef useEffect]]))
 
 (defn- restrict-to-viewport [container selected-index]
   (let [container-rect (.getBoundingClientRect container)
@@ -11,32 +10,20 @@
       (< delta-top 0) (.scrollBy container 0 delta-top)
       (> delta-bottom 0) (.scrollBy container 0 delta-bottom))))
 
-(defclass ResultsViewComponent
-  (extends Component)
+(defn ResultsView [{:keys [resultView results selectedIndex onMouseOver]}]
+  (useEffect (fn []
+               (.log console "huston we have been updated")
+               (restrict-to-viewport ?container? selectedIndex))
+             [selectedIndex])
+  #jsx [:<>
+        [:div
+         {:className "bg-white br1 br--bottom bb bl br b--blue w-100 overflow-y-scroll"
+          :style {:maxHeight "20rem"}
+          :ref #(set! (.-resultsViewNode this) %)}
+         (for [[idx result] (map-indexed vector results)]
+           [:resultView
+             {:result result
+              :isSelected (= selectedIndex idx)
+              :selectResult (fn [] (onMouseOver idx))}]
 
-  Object
-  (componentDidUpdate [this prev-props _prev-state]
-                      (let [current-props (.-props this)
-                            prev-selected (.-selectedIndex prev-props)
-                            current-selected (.-selectedIndex current-props)]
-                        (when (and (not= current-selected prev-selected)
-                                   (.-resultsViewNode this))
-                          (restrict-to-viewport (.-resultsViewNode this) current-selected))))
-
-  Object
-  (render [this props]
-          (let [ResultView (.-resultView props)
-                results (.-results props)
-                selected-index (.-selectedIndex props)
-                on-mouse-over (.-onMouseOver props)]
-            #jsx [:<>
-                  [:div
-                   {:className "bg-white br1 br--bottom bb bl br b--blue w-100 overflow-y-scroll"
-                    :style {:maxHeight "20rem"}
-                    :ref #(set! (.-resultsViewNode this) %)}
-                   (for [[idx result] (map-indexed vector results)]
-                     [ResultView
-                      {:key (str "result-" idx)
-                       :result result
-                       :isSelected (= idx selected-index)
-                       :selectResult #(on-mouse-over idx)}])]])))
+           )]])
