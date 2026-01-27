1abba57b790094f2c0b0389ff01e520ce70454a3
lread <lee@dlread.com>
2026-01-27 11:36:15 -0500

    ops: cleanup: delete old cljdoc host
    I had accidentally confused Exoscale with duplicate ssh keys.
    So I reverted to base and additional ssh keys.
    Which is fine.

--- ops/exoscale/infrastructure/main.tf
+++ ops/exoscale/infrastructure/main.tf
@@ -12,17 +12,13 @@ resource "exoscale_elastic_ip" "cljdoc_prod" {
   zone = var.exoscale_zone
 }
 
-# TODO: delete afer migrating to ssh_keys
-# the compute instance only allows a single ssh key to be specified
-# additional_authorized_keys are setup via cloud init
-# see compute/main.tf
 resource "exoscale_ssh_key" "cljdoc_base_ssh_key" {
   name = "cljdoc-base-ssh"
   public_key = var.base_authorized_key
 }
 
 resource "exoscale_ssh_key" "cljdoc_ssh_keys" {
-  for_each   = var.authorized_keys
+  for_each   = var.additional_authorized_keys
   name       = "${each.key}"
   public_key = each.value
 }
@@ -49,23 +45,6 @@ module "dns" {
   ip_address  = exoscale_elastic_ip.cljdoc_prod.ip_address
 }
 
-# TODO: delete after migrating to cljdoc_03
-module "cljdoc_02" {
-  name = "cljdoc.org"
-  source = "./compute-legacy"
-  template_id = data.exoscale_template.debian.id
-  instance_type = "standard.large"
-  disk_size = 50
-  exoscale_zone = var.exoscale_zone
-  security_group_ids = [module.firewall.security_group_id]
-  base_authorized_key = var.base_authorized_key
-  additional_authorized_keys = var.additional_authorized_keys
-  # unhook from prod routing
-  # elastic_ip_id = exoscale_elastic_ip.cljdoc_prod.id
-  # elastic_ip_address = exoscale_elastic_ip.cljdoc_prod.ip_address
-  ssh_key_id = exoscale_ssh_key.cljdoc_base_ssh_key.id
-}
-
 module "cljdoc_03" {
   name = "cljdoc.org"
   source = "./compute"
@@ -76,7 +55,10 @@ module "cljdoc_03" {
   security_group_ids = [module.firewall.security_group_id]
   elastic_ip_id = exoscale_elastic_ip.cljdoc_prod.id
   elastic_ip_address = exoscale_elastic_ip.cljdoc_prod.ip_address
-  ssh_key_ids = [for k in exoscale_ssh_key.cljdoc_ssh_keys : k.id]
+  ssh_key_ids = concat(
+    [exoscale_ssh_key.cljdoc_base_ssh_key.id],
+    [for k in exoscale_ssh_key.cljdoc_ssh_keys : k.id]
+  )
 }
 
 # Outputs
@@ -92,10 +74,6 @@ output "cljdoc_backups_bucket_key" {
 output "cljdoc_backups_bucket_secret" {
   sensitive = true
   value = module.backups_bucket.write_secret
-}
-
-output "cljdoc_02_instance_ip" {
-  value = module.cljdoc_02.instance_ip
 }
 
 output "cljdoc_03_instance_ip" {
