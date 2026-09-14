f02fa780df719d874cdca7fe51767a5ad4f84b94
lread <lee@dlread.com>
2026-09-14 13:02:39 -0400

    new bb: apply to clean task
    Use new bb cli support and move work into script file.

---
+++ script/clean.clj
@@ -0,0 +1,17 @@
+(ns clean
+  (:require [babashka.fs :as fs]
+            [lread.status-line :as status]))
+
+(defn task [_opts]
+  (status/line :detail "Deleting (d=deleted -=did not exist)")
+  (run! (fn [d]
+          (status/line :detail "[%s] %s"
+                       (if (fs/exists? d) "d" "-")
+                       d)
+          (fs/delete-tree d))
+        ["target"
+         "resources-compiled"
+         ".cpcache"
+         "modules/deploy/.cpcache"
+         "test-data/server"
+         "test-data/cli"]))
