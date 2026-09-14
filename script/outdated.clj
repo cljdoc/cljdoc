45eb66ae57786c2b478049893ecf88f95e9c7360
lread <lee@dlread.com>
2026-09-14 18:44:11 -0400

    new bb: migrate more tasks
    update-github-emojis, outdated, doc-update-readme

--- script/outdated.clj
+++ script/outdated.clj
@@ -1,11 +1,8 @@
-#!/usr/bin/env bb
-
 (ns outdated
   (:require [babashka.fs :as fs]
             [clj-commons.format.table :as table]
             [clojure.edn :as edn]
             [clojure.string :as string]
-            [helper.main :as main]
             [helper.shell :as shell]
             [lread.status-line :as status]))
 
@@ -57,10 +54,7 @@
            (table/print-table [:file :npm-name :current :latest :note]))
       (status/line :detail "All CDN JavaScript deps seem up to date."))))
 
-(defn -main []
+(defn task [opts]
   (check-clojure)
   (check-npm-js)
   (check-cdn-js))
-
-(main/when-invoked-as-script
- (apply -main *command-line-args*))
