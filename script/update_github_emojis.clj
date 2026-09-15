6f39fa55b01ea321e7c307d05a5fe5613a08cf6a
lread <lee@dlread.com>
2026-09-15 16:50:20 -0400

    typo

--- script/update_github_emojis.clj
+++ script/update_github_emojis.clj
@@ -59,7 +59,7 @@
           (str/split-lines html)))
 
 (defn task [_opts]
-  (status/line :head "Updating github emojis resouce")
+  (status/line :head "Updating github emojis resource")
   (let [emojis (fetch-emojis)
         rendered (-> emojis render-emojis parse-rendered-emojis)
         joined (reduce (fn [acc [k v]]
